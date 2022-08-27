b23ab4c68a3243d56562dd430c4cc5a2a02e58b4
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-08-27 23:31:36 +0900

    P10757, P11021, P24309, P25083

---
+++ P10757번_큰_수_A더하기B/Main.java
@@ -0,0 +1,20 @@
+package P10757번_큰_수_A더하기B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.math.BigInteger;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      
+      BigInteger bigNumber1 = new BigInteger(st.nextToken());
+      BigInteger bigNumber2 = new BigInteger(st.nextToken());
+
+		System.out.println(bigNumber1.add(bigNumber2));
+	}
+}
