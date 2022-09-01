c4502416b1df1a7e1d71b51960d781b4c1299eb6
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-09-02 04:11:17 +0900

    Gold V 859

--- P1343번_폴리오미노/Main copy.java
+++ P1343번_폴리오미노/Main copy.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main2 {
+class Main {
 	public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
     StringBuilder sb = new StringBuilder();
