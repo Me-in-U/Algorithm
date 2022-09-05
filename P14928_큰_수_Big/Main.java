ef520dfe1ac1df9fb91d4f0b98f5334e113f503b
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-09-06 00:25:12 +0900

    Gold V 887

---
+++ P14928_큰_수_Big/Main.java
@@ -0,0 +1,19 @@
+package P14928_큰_수_Big;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String input = br.readLine();
+    int divider = 20000303;
+    int result = 0;
+    for(int i = 0; i < input.length(); i++){
+      result = (result * 10 + (input.charAt(i) - '0')) % divider;
+    }
+    
+    System.out.println(result);
+  }
+}
---
+++ P16212번_정열적인_장랼/Main.java
@@ -0,0 +1,26 @@
+package P16212번_정열적인_장랼;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int arr[] = new int[N];
+		for(int i = 0; i < N; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+		}
+    Arrays.sort(arr);
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < N; i++){
+      sb.append(arr[i] + " ");
+		}
+    System.out.println(sb.toString());
+	}
+}
