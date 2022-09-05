5781600c4bf393f0a2a268d53496462583371e40
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-09-06 01:33:37 +0900

    Gold V 893

---
+++ P1748번_수_이어_쓰기/Main.java
@@ -0,0 +1,19 @@
+package P1748번_수_이어_쓰기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+    int N = Integer.parseInt(br.readLine());
+    int result = 0;
+
+    for(int i = 1; i <= N; i *= 10){
+      result += (N - i) + 1;
+    }
+    System.out.println(result); 
+	}
+}
