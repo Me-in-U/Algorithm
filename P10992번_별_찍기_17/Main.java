b2fc4fb0d55b1275667e04e1e32a29107eace7e9
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-09-21 15:31:55 +0900

    Gold V 946

---
+++ P10992번_별_찍기_17/Main.java
@@ -0,0 +1,47 @@
+package P10992번_별_찍기_17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+  public static int N;
+  public static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    sb = new StringBuilder();
+
+    N = Integer.parseInt(br.readLine());
+    first_line();
+    if(N > 1){
+      mid_line();
+      last_line();
+    }
+
+    System.out.print(sb.toString());
+	}
+  public static void first_line(){
+    for(int i = 0; i < N - 1; i++) {
+      sb.append(" ");
+    }
+    sb.append("*").append('\n');
+  }
+  public static void mid_line(){
+    for(int i = 0; i < N - 2; i++){
+      for(int k = 0; k < N - 2 - i; k++){
+        sb.append(" ");
+      }
+      sb.append("*");
+      for(int k = 0; k < 1 + (2 * i); k++){
+        sb.append(" ");
+      }
+      sb.append("*").append('\n');
+    }
+  }
+  public static void last_line(){
+    for(int i = 0; i < N * 2 - 1; i++){
+      sb.append("*");
+    }
+    sb.append('\n');
+  }
+}
---
+++ P11053번_가장_긴_증가하는_부분수열/Main.java
@@ -0,0 +1,38 @@
+package P11053번_가장_긴_증가하는_부분수열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+
+		int N = Integer.parseInt(br.readLine());
+
+		int num[] = new int[N + 1];
+		int dp[] = new int[N + 1];
+
+    st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++){
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for(int i = 0; i < N; i++){
+			dp[i] = 1;
+			for(int k = 0; k < i; k++){
+				if(num[i] > num[k]){
+					dp[i] = Math.max(dp[i], dp[k] + 1);
+				}
+			}
+		}
+		
+		int result = 0;
+		for(int r : dp){
+			result = Math.max(r, result);
+		}
+		System.out.println(result);
+	}
+}
