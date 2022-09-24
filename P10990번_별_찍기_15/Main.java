2720ae640919a8f71a345142ea1a20b20869160d
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-09-24 21:26:38 +0900

    Gold IV 951

---
+++ P10990번_별_찍기_15/Main.java
@@ -0,0 +1,30 @@
+package P10990번_별_찍기_15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0; i < N; i++){
+      //좌삼각
+      for(int j = 0; j < N - 1 - i; j++) {
+        sb.append(" ");
+      }
+      sb.append("*");
+      //우삼각
+      for(int j = i * 2 - 1; j > 0; j--){
+        sb.append(" ");
+      }
+      if(i >= 1){
+        sb.append("*");
+      }
+      sb.append('\n');
+		}
+    System.out.print(sb.toString());
+	}
+}
---
+++ P5338번_마이크로소프트_로고/Main.java
@@ -0,0 +1,13 @@
+package P5338번_마이크로소프트_로고;
+
+class Main {
+	public static void main(String[] args){
+    StringBuilder sb = new StringBuilder();
+    sb.append("       _.-;;-._").append('\n');
+    sb.append("'-..-'|   ||   |").append('\n');
+    sb.append("'-..-'|_.-;;-._|").append('\n');
+    sb.append("'-..-'|   ||   |").append('\n');
+    sb.append("'-..-'|_.-''-._|").append('\n');
+    System.out.println(sb.toString());
+	}
+}
