cbb20e68e5cdbcc5513c8355b40ba342aa6ecf96
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-10-15 22:46:54 +0900

    Gold IV 1009

---
+++ P2506번_점수계산/Main.java
@@ -0,0 +1,26 @@
+package P2506번_점수계산;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+
+    int sum = 0;
+    int score = 1;
+    int N = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      if (Integer.parseInt(st.nextToken()) == 1) {
+        sum += score++;
+      } else {
+        score = 1;
+      }
+    }
+    System.out.println(sum);
+  }
+}
