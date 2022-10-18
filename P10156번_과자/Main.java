37927be238b4877063cf8ffd9b62a3cbccae6b50
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-10-18 23:58:16 +0900

    Gold IV 1011

---
+++ P10156번_과자/Main.java
@@ -0,0 +1,23 @@
+package P10156번_과자;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+
+    st = new StringTokenizer(br.readLine());
+    int c = Integer.parseInt(st.nextToken());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int ans = (c * n) - m;
+    if (ans > 0)
+      System.out.println(ans);
+    else
+      System.out.println(0);
+  }
+}
