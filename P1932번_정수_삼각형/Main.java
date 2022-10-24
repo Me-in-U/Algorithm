731c708f9c7b26ca2a5594af59b5bc5244fa5df9
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-10-24 13:13:33 +0900

    Gold IV 1016

---
+++ P1932번_정수_삼각형/Main.java
@@ -0,0 +1,45 @@
+package P1932번_정수_삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+
+    int n = Integer.parseInt(br.readLine());
+    int[][] dp = new int[n + 1][n + 1];
+    int[][] cost = new int[n + 1][n + 1];
+
+    for (int i = 1; i <= n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 1; j <= i; j++) {
+        cost[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    dp[1][1] = cost[1][1];
+
+    for (int i = 2; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        if (j == 1) {
+          dp[i][j] = cost[i][j] + dp[i - 1][j];
+        } else {
+          dp[i][j] = cost[i][j] + Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
+        }
+      }
+    }
+
+    int max = 0;
+    for (int i = 1; i <= n; i++) {
+      if (dp[n][i] > max) {
+        max = dp[n][i];
+      }
+    }
+
+    System.out.println(max);
+  }
+}
