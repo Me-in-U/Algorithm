df5db5de5fed554d633906e81466e49ef9c9e73e
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-10-27 00:08:49 +0900

    Gold IV 1021

---
+++ P2407번_조합/Main.java
@@ -0,0 +1,23 @@
+package P2407번_조합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    BigInteger result = BigInteger.valueOf(1);
+    BigInteger div = BigInteger.valueOf(1);
+    for (; m > 0; n--, m--) {
+      result = result.multiply(BigInteger.valueOf(n));
+      div = div.multiply(BigInteger.valueOf(m));
+    }
+    System.out.println(result.divide(div));
+  }
+}
