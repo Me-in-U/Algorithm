bdcb9de0333e7c3dd1ebe976412afe2bcdb2a310
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-10-28 23:40:39 +0900

    Gold IV 1025

---
+++ P11727번_2xn_타일링_2/Main.java
@@ -0,0 +1,20 @@
+package P11727번_2xn_타일링_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    long[] dp = new long[1001];
+    dp[1] = 1;
+    dp[2] = 3;
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 3; i <= n; i++) {
+      dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+    }
+    System.out.println(dp[n]);
+  }
+}
