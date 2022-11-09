f4a24725e0f83b508b64128ac2b6ae24553774fe
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-11-09 18:17:06 +0900

    Gold IV 1067

--- P15656번_N과_M_7/Main.java
+++ P15656번_N과_M_7/Main.java
@@ -1,65 +1,51 @@
-package P15666번_N과_M_12;
+package P15656번_N과_M_7;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Main {
+
   protected static int N, M;
   protected static int[] num;
+  protected static StringBuilder sb = new StringBuilder();
+  protected static boolean[] visited;
   protected static int[] output;
-  protected static StringBuilder sb = null;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = null;
-    HashSet<Integer> num_set = null;
 
     st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-
     num = new int[N];
     output = new int[M];
-    num_set = new HashSet<>(N);
-    sb = new StringBuilder();
+    visited = new boolean[N];
 
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
       num[i] = Integer.parseInt(st.nextToken());
     }
-    Iterator<Integer> iter = num_set.iterator();
-    int index = 0;
-    while (iter.hasNext()) {
-      num[index++] = iter.next();
-    }
-
     Arrays.sort(num);
-
-    dfs(0, 0);
+    dfs(0);
 
     System.out.print(sb.toString());
   }
 
-  public static void dfs(int start, int depth) {
+  public static void dfs(int depth) {
     if (depth == M) {
-      for (int i = 0; i < M; i++) {
-        sb.append(output[i] + " ");
+      for (int val : output) {
+        sb.append(Integer.toString(val)).append(" ");
       }
-      sb.append("\n");
+      sb.append('\n');
       return;
     }
-    int check = -1;
-    for (int i = start; i < N; i++) {
-      if (check != num[i]) {
-        output[depth] = num[i];
-        dfs(i, depth + 1);
-        check = num[i];
-      }
+    for (int i = 0; i < N; i++) {
+      output[depth] = num[i];
+      dfs(depth + 1);
     }
   }
 }
