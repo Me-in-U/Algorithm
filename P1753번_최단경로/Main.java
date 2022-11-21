6c514ddf01c515b3f1325909ea8d5cc8ec5ff32d
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-11-22 05:18:46 +0900

    Gold III 1119

--- P1753번_최단경로/Main.java
+++ P1753번_최단경로/Main.java
@@ -9,75 +9,28 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Main {
-  private static ArrayList<Node>[] list;
+  private static ArrayList<Node>[] node_List;
   private static int V, E, K;
   private static int INF = Integer.MAX_VALUE;
   private static int[] distance;
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = null;
-
-    // 변수 설정
-    st = new StringTokenizer(br.readLine());
-    V = Integer.parseInt(st.nextToken());
-    E = Integer.parseInt(st.nextToken());
-    K = Integer.parseInt(br.readLine());
-
-    list = new ArrayList[V + 1];
-    for (int i = 1; i <= V; i++) {
-      // list인덱스마다 ArrayList생성
-      list[i] = new ArrayList<>();
-    }
-    distance = new int[V + 1];
-    Arrays.fill(distance, INF);
-    distance[K] = 0;
-    for (int i = 0; i < E; i++) {
-      st = new StringTokenizer(br.readLine());
-      int u = Integer.parseInt(st.nextToken());
-      int v = Integer.parseInt(st.nextToken());
-      int w = Integer.parseInt(st.nextToken());
-      // u -> v (가중치 w)
-      list[u].add(new Node(v, w));
-      // list[1] -> 2, w = 2
-      // list[1] -> 3, w = 3
-      // list[2] -> 3, w = 4
-      // list[2] -> 4, w = 5
-      // list[3] -> 4, w = 6
-      // list[5] -> 1, w = 1
-    }
-
-    dijkstra();
-
-    // 출력
-    for (int i = 1; i <= V; i++) {
-      if (distance[i] == INF) {
-        System.out.println("INF");
-      } else {
-        System.out.println(distance[i]);
-      }
-    }
-  }
-
   private static void dijkstra() {
     PriorityQueue<Node> pq = new PriorityQueue<>();
-    // K -> K (가중치 0) 큐에 추가
-    // new Node(1, 0)
+    Arrays.fill(distance, INF);
+    distance[K] = 0;
     pq.add(new Node(K, 0));
     while (!pq.isEmpty()) {
       Node node = pq.poll();
-      int vertex = node.vertex;
-      int weight = node.weight;
-
-      if (distance[vertex] < weight) {
+      int nodeVertex = node.vertex;
+      int nodeWeight = node.weight;
+      if (nodeWeight > distance[nodeVertex])
         continue;
-      }
-      for (int i = 0; i < list[vertex].size(); i++) {
-        int vertex2 = list[vertex].get(i).vertex;
-        int weight2 = list[vertex].get(i).weight + weight;
-        if (distance[vertex2] > weight2) {
-          distance[vertex2] = weight2;
-          pq.add(new Node(vertex2, weight2));
+      for (Node nextNode : node_List[nodeVertex]) {
+        int vertex_update = nextNode.vertex;
+        int distance_update = nextNode.weight + distance[nodeVertex];
+        if (distance[vertex_update] > distance_update) {
+          distance[vertex_update] = distance_update;
+          pq.add(new Node(vertex_update, distance_update));
         }
       }
     }
@@ -96,5 +49,43 @@ public class Main {
     public int compareTo(Node o) {
       return this.weight - o.weight;
     }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+
+    // 변수 설정
+    st = new StringTokenizer(br.readLine());
+    V = Integer.parseInt(st.nextToken());
+    E = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(br.readLine());
+
+    distance = new int[V + 1];
+    node_List = new ArrayList[V + 1];
+    for (int i = 1; i <= V; i++) {
+      node_List[i] = new ArrayList<>();
+    }
+
+    for (int i = 0; i < E; i++) {
+      st = new StringTokenizer(br.readLine());
+      int u = Integer.parseInt(st.nextToken());
+      int v = Integer.parseInt(st.nextToken());
+      int w = Integer.parseInt(st.nextToken());
+      node_List[u].add(new Node(v, w));
+    }
+
+    dijkstra();
+
+    // 출력
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= V; i++) {
+      if (distance[i] == INF) {
+        sb.append("INF").append('\n');
+      } else {
+        sb.append(distance[i]).append('\n');
+      }
+    }
+    System.out.println(sb.toString());
   }
 }
---
+++ P18405번_경쟁적_전염/Main.java
@@ -0,0 +1,99 @@
+package P18405번_경쟁적_전염;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+  private static int[][] arr;
+  private static boolean[][] visited;
+  // 상, 하, 좌, 우
+  private static int[] dx = { -1, 1, 0, 0 };
+  private static int[] dy = { 0, 0, -1, 1 };
+
+  private static int N, K, time;
+  private static int S, X, Y;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+
+    st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+
+    PriorityQueue<Point> pq = new PriorityQueue<>();
+    arr = new int[N][N];
+    visited = new boolean[N][N];
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        int num = Integer.parseInt(st.nextToken());
+        arr[i][j] = num;
+        if (num != 0) {
+          pq.add(new Point(i, j));
+          visited[i][j] = true;
+        }
+      }
+    }
+
+    st = new StringTokenizer(br.readLine());
+    S = Integer.parseInt(st.nextToken());
+    X = Integer.parseInt(st.nextToken());
+    Y = Integer.parseInt(st.nextToken());
+
+    if (S != 0) {
+      BFS(pq);
+    }
+    System.out.println(arr[X - 1][Y - 1]);
+  }
+
+  public static void BFS(PriorityQueue<Point> pq) {
+    PriorityQueue<Point> temp = new PriorityQueue<>();
+    while (!pq.isEmpty()) {
+      Point now = pq.poll();
+      // System.out.println("cordinates | " + now.x + ", " + now.y + " | " +
+      // arr[now.x][now.y]);
+      for (int k = 0; k < 4; k++) {
+        int x = now.x + dx[k];
+        int y = now.y + dy[k];
+        if (x >= 0 && y >= 0 && x < N && y < N) {
+          if (arr[x][y] == 0 && !visited[x][y]) {
+            visited[x][y] = true;
+            arr[x][y] = arr[now.x][now.y];
+            temp.add(new Point(x, y));
+          }
+        }
+      }
+    }
+    if (!temp.isEmpty()) {
+      // System.out.println("out | T = " + (time + 1));
+      // for (int i = 0; i < N; i++) {
+      // for (int j = 0; j < N; j++) {
+      // System.out.print(arr[i][j] + " ");
+      // }
+      // System.out.println();
+      // }
+      if (++time != S) {
+        BFS(temp);
+      }
+    }
+  }
+
+  private static class Point implements Comparable<Point> {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+      return arr[this.x][this.y] - arr[o.x][o.y];
+    }
+  }
+}
