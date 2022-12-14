7c3a0ce595547c34ad292c74004ed28f22d8d3be
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-12-14 18:12:48 +0900

    Gold III 1172

---
+++ P5598번_카이사르_암호/Main.java
@@ -0,0 +1,23 @@
+package P5598번_카이사르_암호;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    String line = br.readLine();
+    for (int i = 0; i < line.length(); i++) {
+      char a = line.charAt(i);
+      if (a - 3 < 'A')
+        a += 23;
+      else
+        a -= 3;
+      sb.append(a);
+    }
+    System.out.println(sb.toString());
+  }
+}
