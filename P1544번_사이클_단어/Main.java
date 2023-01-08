12014ec5af12a34a17d5e9701c38662c8d7c3fc8
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2023-01-08 23:48:42 +0900

    Gold III 1191

---
+++ P1544번_사이클_단어/Main.java
@@ -0,0 +1,32 @@
+package P1544번_사이클_단어;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    HashSet<String> set = new HashSet<String>();
+    int N = Integer.parseInt(br.readLine());
+    int count = N;
+    for (int i = 0; i < N; i++) {
+      char[] word = br.readLine().toCharArray();
+      for (int j = 0; j < word.length; j++) {
+        if (!set.contains(String.valueOf(word))) {
+          char first = word[0];
+          for (int k = 1; k < word.length; k++) {
+            word[k - 1] = word[k];
+          }
+          word[word.length - 1] = first;
+        } else {
+          count--;
+          break;
+        }
+      }
+      set.add(String.valueOf(word));
+    }
+    System.out.println(count);
+  }
+}
---
+++ p5585번_거스름돈/Main.java
@@ -0,0 +1,23 @@
+package p5585번_거스름돈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int remains = 1000 - N;
+    int[] coins = { 500, 100, 50, 10, 5, 1 };
+    int count = 0;
+    for (int i = 0; i < 6; i++) {
+      if (remains != 0) {
+        int coin_count = remains / coins[i];
+        remains -= coins[i] * (coin_count);
+        count += coin_count;
+      }
+    }
+    System.out.println(count);
+  }
+}
