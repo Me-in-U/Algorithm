9db5ed703e2389c3b9c005324a8527a6cee9112d
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2023-01-12 04:02:34 +0900

    Gold III 1192

---
+++ P10101번_삼각형_외우기/Main.java
@@ -0,0 +1,26 @@
+package P10101번_삼각형_외우기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] angles = new int[3];
+    angles[0] = Integer.parseInt(br.readLine());
+    angles[1] = Integer.parseInt(br.readLine());
+    angles[2] = Integer.parseInt(br.readLine());
+    if ((angles[0] + angles[1] + angles[2]) == 180) {
+      if (angles[0] == 60 && angles[1] == 60 && angles[2] == 60) {
+        System.out.println("Equilateral");
+      } else if (angles[0] == angles[1] || angles[1] == angles[2] || angles[2] == angles[0]) {
+        System.out.println("Isosceles");
+      } else {
+        System.out.println("Scalene");
+      }
+    } else {
+      System.out.println("Error");
+    }
+  }
+}
