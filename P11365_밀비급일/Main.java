5d2a1a7170c445885cd2c449b484af2b3490f187
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2023-01-13 21:07:56 +0900

    Gold III 1193

---
+++ P11365_밀비급일/Main.java
@@ -0,0 +1,20 @@
+package P11365_밀비급일;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    String line = "";
+    while (!(line = br.readLine()).equals("END")) {
+      for (int i = line.length() - 1; i >= 0; i--) {
+        sb.append(line.charAt(i));
+      }
+      sb.append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
