21b3c5ae9e1de156b69b16a1ea09ba5d97657e65
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-01-21 02:29:23 +0900

    Gold III 1198

---
+++ P5524번_입실_관리/Main.java
@@ -0,0 +1,17 @@
+package P5524번_입실_관리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    for (int i = 0; i < N; i++) {
+      sb.append(br.readLine().toLowerCase()).append('\n');
+    }
+    System.out.print(sb);
+  }
+}
