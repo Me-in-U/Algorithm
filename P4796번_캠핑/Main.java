a09f18fc1c55962eb46c82e9ad5544ba5d7b5536
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-01-28 20:37:24 +0900

    Gold III 1200

---
+++ P4796번_캠핑/Main.java
@@ -0,0 +1,25 @@
+package P4796번_캠핑;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1;; i++) {
+      st = new StringTokenizer(br.readLine());
+      int L = Integer.parseInt(st.nextToken());
+      int P = Integer.parseInt(st.nextToken());
+      int V = Integer.parseInt(st.nextToken());
+      if (L == 0 && P == 0 && V == 0) {
+        System.out.print(sb);
+        break;
+      }
+      sb.append("Case " + i + ": " + (((V / P) * L) + Math.min(V % P, L))).append('\n');
+    }
+  }
+}
