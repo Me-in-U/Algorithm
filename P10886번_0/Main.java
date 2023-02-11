302f28cfa5ddcf3f7dc0d7b68141ffa382f03032
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-02-12 04:18:56 +0900

    Gold II 1271

---
+++ P10886번_0/Main.java
@@ -0,0 +1,21 @@
+package P10886번_0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] data = new int[2];
+    for (int i = 0; i < N; i++) {
+      data[Integer.parseInt(br.readLine())]++;
+    }
+    if (data[0] > data[1]) {
+      System.out.println("Junhee is not cute!");
+    } else {
+      System.out.println("Junhee is cute!");
+    }
+  }
+}
