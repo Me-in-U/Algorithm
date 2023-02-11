0e15465c0436bf19ce58f90bde78b190df78aeec
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-02-11 13:29:26 +0900

    Gold II 1271

---
+++ P15439번_Vera_and_Outfits/Main.java
@@ -0,0 +1,13 @@
+package P15439번_Vera_and_Outfits;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    System.out.println(N * (N - 1));
+  }
+}
