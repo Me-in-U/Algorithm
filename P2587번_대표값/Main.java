2d27095cf46ff0e6a99275344d5cf1c3bf9ab5c0
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-02-13 22:49:33 +0900

    Gold II 1271

---
+++ P2587번_대표값/Main.java
@@ -0,0 +1,21 @@
+package P2587번_대표값;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] input = new int[5];
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+      input[i] = Integer.parseInt(br.readLine());
+      sum += input[i];
+    }
+    Arrays.sort(input);
+    System.out.println(sum / 5);
+    System.out.println(input[2]);
+  }
+}
