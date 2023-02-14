c28737236b8a7f5e8118fc84eb757ef070437e3c
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-02-14 12:39:25 +0900

    Gold II 1271

---
+++ P2789번_유학_금지/Main.java
@@ -0,0 +1,17 @@
+package P2789번_유학_금지;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String input = br.readLine();
+    char[] CAMBRIDGE = "CAMBRIDGE".toCharArray();
+    for (int i = 0; i < CAMBRIDGE.length; i++) {
+      input = input.replace(String.valueOf(CAMBRIDGE[i]), "");
+    }
+    System.out.print(input);
+  }
+}
