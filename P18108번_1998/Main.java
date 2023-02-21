0fe1f3c0b111c54377235d11e8ba750e30393c01
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-02-22 06:56:34 +0900

    Gold II 1274

---
+++ P18108번_1998/Main.java
@@ -0,0 +1,12 @@
+package P18108번_1998;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println(Integer.parseInt(br.readLine()) - 543);
+  }
+}
---
+++ P2711번_오타맨_고창영/Main.java
@@ -0,0 +1,27 @@
+package P2711번_오타맨_고창영;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int miss = Integer.parseInt(st.nextToken());
+      char[] input = st.nextToken().toCharArray();
+      for (int j = 1; j <= input.length; j++) {
+        if (j != miss) {
+          sb.append(input[j - 1]);
+        }
+      }
+      sb.append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
