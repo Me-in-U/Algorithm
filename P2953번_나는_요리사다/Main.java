bd512c1690a7f85148a1e1b84bddf3b9861ceaa9
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-02-25 04:48:23 +0900

    Gold II 1274

---
+++ P2953번_나는_요리사다/Main.java
@@ -0,0 +1,27 @@
+package P2953번_나는_요리사다;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+    int num = 0;
+    int max = 0;
+    for (int i = 1; i <= 5; i++) {
+      st = new StringTokenizer(br.readLine());
+      int sum = 0;
+      for (int j = 0; j < 4; j++) {
+        sum += Integer.parseInt(st.nextToken());
+      }
+      if (max < sum) {
+        max = sum;
+        num = i;
+      }
+    }
+    System.out.println(num + " " + max);
+  }
+}
