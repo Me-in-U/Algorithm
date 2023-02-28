47bc87e7dbe783d502847dd86282651c561ab7e4
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-03-01 05:38:15 +0900

    Gold II 1275

---
+++ P11283번_한글2/Main.java
@@ -0,0 +1,13 @@
+package P11283번_한글2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    char hangul = br.readLine().charAt(0);
+    System.out.println((int) hangul - 44031);
+  }
+}
