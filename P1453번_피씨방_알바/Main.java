8d25e9c49c331b4cf569852a288c3cabe0f79436
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-03-02 05:41:28 +0900

    Gold II 1275

---
+++ P1453번_피씨방_알바/Main.java
@@ -0,0 +1,25 @@
+package P1453번_피씨방_알바;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    boolean[] seats = new boolean[100];
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+      int num = Integer.parseInt(st.nextToken()) - 1;
+      if (!seats[num]) {
+        seats[num] = true;
+      } else {
+        count++;
+      }
+    }
+    System.out.println(count);
+  }
+}
