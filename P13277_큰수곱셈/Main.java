c90a4314aef6d52827594864196bab28a68e6c89
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-03-11 19:47:30 +0900

    Gold II 1276

---
+++ P13277_큰수곱셈/Main.java
@@ -0,0 +1,16 @@
+package P13277_큰수곱셈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] input = br.readLine().split(" ");
+    BigInteger big1 = new BigInteger(input[0]);
+    BigInteger big2 = new BigInteger(input[1]);
+    System.out.println(big1.multiply(big2));
+  }
+}
