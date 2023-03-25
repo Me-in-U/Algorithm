fb877b443621d27869b091a5f975b26f476947d7
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-03-26 04:01:11 +0900

    Gold II 1289

---
+++ P15740번_AB_9/Main.java
@@ -0,0 +1,16 @@
+package P15740번_AB_9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] input = br.readLine().split(" ");
+    BigInteger num1 = new BigInteger(input[0]);
+    BigInteger num2 = new BigInteger(input[1]);
+    System.out.println(num1.add(num2));
+  }
+}
