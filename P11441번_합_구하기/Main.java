97fb19e82e3b2ff344d9c8bb7cade292bf1b9745
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-04-06 02:27:26 +0900

    Gold II 1303

---
+++ P11441번_합_구하기/Main.java
@@ -0,0 +1,28 @@
+package P11441번_합_구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    int[] sum = new int[N + 1];
+    sum[0] = 0;
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <= N; i++) {
+      sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+    }
+    int M = Integer.parseInt(br.readLine());
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+      sb.append(sum[to] - sum[from - 1]).append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
