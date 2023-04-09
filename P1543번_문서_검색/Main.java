5c5e26cf8fdf3b8b9dcf3bb0275b75a328fba67e
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-04-10 00:38:20 +0900

    Gold II 1303

---
+++ P1543번_문서_검색/Main.java
@@ -0,0 +1,30 @@
+package P1543번_문서_검색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line = br.readLine();
+    String find = br.readLine();
+    int count = 0;
+    // System.out.println("");
+    for (int i = 0; i < find.length(); i++) {
+      // System.out.println(i + ": " + "find");
+      int temp_count = 0;
+      for (int j = i; j < line.length() - find.length() + 1;) {
+        // System.out.println(line.substring(j, j + find.length()));
+        if (line.substring(j, j + find.length()).equals(find)) {
+          temp_count++;
+          j += find.length();
+        } else {
+          j++;
+        }
+      }
+      count = Math.max(count, temp_count);
+    }
+    System.out.println(count);
+  }
+}
