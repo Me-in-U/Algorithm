500f4715da05bca48d2375dfad3c12f6895b177e
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-04-23 05:04:15 +0900

    Gold II 1328

---
+++ P2738번_행렬덧셈/Main.java
@@ -0,0 +1,31 @@
+package P2738번_행렬덧셈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    int[][] NM = new int[N][M];
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < M; j++) {
+        NM[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < M; j++) {
+        sb.append((NM[i][j] + Integer.parseInt(st.nextToken())) + " ");
+      }
+      sb.append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
