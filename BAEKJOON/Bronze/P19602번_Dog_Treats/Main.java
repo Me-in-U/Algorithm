0fcf47e7aa835c6afaae7fe611467637d345da93
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-05-23 06:42:44 +0900

    2023-05-23

---
+++ BAEKJOON/Bronze/P19602번_Dog_Treats/Main.java
@@ -0,0 +1,16 @@
+package P19602번_Dog_Treats;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    if (1 * Integer.parseInt(br.readLine()) + 2 * Integer.parseInt(br.readLine())
+        + 3 * Integer.parseInt(br.readLine()) >= 10)
+      System.out.println("happy");
+    else
+      System.out.println("sad");
+  }
+}
