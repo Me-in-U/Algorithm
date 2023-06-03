99dfaed267fa0e1244cac310672b538a373dbda5
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-06-04 06:00:13 +0900

    2023-06-04

---
+++ BAEKJOON/Bronze/P24860번_Counting_Antibodies/Main.java
@@ -0,0 +1,17 @@
+package BAEKJOON.Bronze.P24860번_Counting_Antibodies;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    long[] vj1 = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+    long[] vj2 = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+    long[] vdj = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+    long answer = (vdj[0] * vdj[1] * vdj[2]) * (vj1[0] * vj1[1] + vj2[0] * vj2[1]);
+    System.out.println(answer);
+  }
+}
