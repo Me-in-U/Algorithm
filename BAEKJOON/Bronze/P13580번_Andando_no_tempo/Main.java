d1592c852441776d71d130b7a1040bf3abb28865
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-06-09 05:36:16 +0900

    2023-06-09

---
+++ BAEKJOON/Bronze/P13580번_Andando_no_tempo/Main.java
@@ -0,0 +1,18 @@
+package P13580번_Andando_no_tempo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+    if (input[0] == input[1] || input[1] == input[2] || input[0] + input[1] == input[2]) {
+      System.out.println("S");
+    } else {
+      System.out.println("N");
+    }
+  }
+}
