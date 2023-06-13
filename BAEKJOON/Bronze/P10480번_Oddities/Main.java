56f9aecdeb893145d4b9890e7e2dd910a8b5c0d4
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-06-14 05:02:49 +0900

    2023-06-14

---
+++ BAEKJOON/Bronze/P10480번_Oddities/Main.java
@@ -0,0 +1,22 @@
+package P10480번_Oddities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    for (int i = 0; i < N; i++) {
+      int x = Integer.parseInt(br.readLine());
+      if (Math.abs(x) % 2 == 0) {
+        sb.append(x).append(" is even").append('\n');
+      } else {
+        sb.append(x).append(" is odd").append('\n');
+      }
+    }
+    System.out.print(sb.toString());
+  }
+}
