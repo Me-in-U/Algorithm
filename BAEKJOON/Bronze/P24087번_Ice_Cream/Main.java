b3840c3bbb372ed74553a9150e7ba8f5cf9ecebb
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-06-22 05:16:35 +0900

    2023-06-22

---
+++ BAEKJOON/Bronze/P24087번_Ice_Cream/Main.java
@@ -0,0 +1,18 @@
+package BAEKJOON.Bronze.P24087번_Ice_Cream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    float S = Integer.parseInt(br.readLine());
+    float A = Integer.parseInt(br.readLine());
+    float B = Integer.parseInt(br.readLine());
+    if (S <= A)
+      System.out.println(250);
+    else
+      System.out.println((int) (250 + 100 * Math.ceil((S - A) / B)));
+  }
+}
