bb375ca4875e4c85bdf9e0721f4dc5018db8c566
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-06-25 05:00:53 +0900

    2023-06-25

---
+++ BAEKJOON/Bronze/P17903번_Counting_Clauses/Main.java
@@ -0,0 +1,21 @@
+package BAEKJOON.Bronze.P17903번_Counting_Clauses;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] input = br.readLine().split(" ");
+    int m = Integer.parseInt(input[0]);
+    for (int i = 0; i < m; i++) {
+      br.readLine();
+    }
+    if (8 <= m) {
+      System.out.println("satisfactory");
+    } else {
+      System.out.println("unsatisfactory");
+    }
+  }
+}
---
+++ BAEKJOON/Bronze/P24751번_Betting/Main.java
@@ -0,0 +1,14 @@
+package BAEKJOON.Bronze.P24751번_Betting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int a = Integer.parseInt(br.readLine());
+    System.out.println(100.0 / a);
+    System.out.println(100.0 / (100 - a));
+  }
+}
