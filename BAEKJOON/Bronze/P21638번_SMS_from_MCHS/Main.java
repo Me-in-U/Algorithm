c127c1d028f60b95f52ff2a824b1bff3e7a5eff4
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-06-26 23:01:49 +0900

    2023-06-26

---
+++ BAEKJOON/Bronze/P21638번_SMS_from_MCHS/Main.java
@@ -0,0 +1,26 @@
+package BAEKJOON.Bronze.P21638번_SMS_from_MCHS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] input = br.readLine().split(" ");
+    int t1 = Integer.parseInt(input[0]);
+    int v1 = Integer.parseInt(input[1]);
+    input = br.readLine().split(" ");
+    int t2 = Integer.parseInt(input[0]);
+    int v2 = Integer.parseInt(input[1]);
+    if (t2 < 0 && 10 <= v2) {
+      System.out.println("A storm warning for tomorrow! Be careful and stay home if possible!");
+    } else if (t1 > t2) {
+      System.out.println("MCHS warns! Low temperature is expected tomorrow.");
+    } else if (v1 < v2) {
+      System.out.println("MCHS warns! Strong wind is expected tomorrow.");
+    } else {
+      System.out.println("No message");
+    }
+  }
+}
