c817b3da756a11a2f422730217ae91472a22bcfc
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-06-27 22:48:02 +0900

    2023-06-27

---
+++ BAEKJOON/Bronze/P3765번_Celebrity_jeopardy/Main.java
@@ -0,0 +1,18 @@
+package BAEKJOON.Bronze.P3765번_Celebrity_jeopardy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  private static String input;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    while ((input = br.readLine()) != null) {
+      sb.append(input).append("\n");
+    }
+    System.out.print(sb.toString());
+  }
+}
