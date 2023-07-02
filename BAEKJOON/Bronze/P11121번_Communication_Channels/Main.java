3fb6f801b6772d0c7bfd73ca81c35553db1af6e6
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-07-03 01:38:19 +0900

    2023-07-03

---
+++ BAEKJOON/Bronze/P11121번_Communication_Channels/Main.java
@@ -0,0 +1,22 @@
+package BAEKJOON.Bronze.P11121번_Communication_Channels;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
+    for (int i = 0; i < T; i++) {
+      String[] input = br.readLine().split(" ");
+      if (input[0].equals(input[1])) {
+        sb.append("OK").append('\n');
+      } else {
+        sb.append("ERROR").append('\n');
+      }
+    }
+    System.out.print(sb.toString());
+  }
+}
