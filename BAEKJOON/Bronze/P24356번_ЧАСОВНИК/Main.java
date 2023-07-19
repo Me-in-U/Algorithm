fa8853b30ea2aefa3eb49b801b381c6f456d8ded
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-07-19 22:34:32 +0900

    2023-07-19

---
+++ BAEKJOON/Bronze/P24356번_ЧАСОВНИК/Main.java
@@ -0,0 +1,21 @@
+package P24356번_ЧАСОВНИК;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] input = br.readLine().split(" ");
+    int t1 = Integer.parseInt(input[0]);
+    int m1 = Integer.parseInt(input[1]);
+    int t2 = Integer.parseInt(input[2]);
+    int m2 = Integer.parseInt(input[3]);
+    int time = ((t2 - t1) * 60) + (m2 - m1);
+    if (time < 0) {
+      time += 1440;
+    }
+    System.out.println(time + " " + time / 30);
+  }
+}
