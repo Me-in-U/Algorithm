b1f00cabe1df368e894ba99f878ecca3b090b443
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-07-22 22:45:49 +0900

    2023-07-21

---
+++ BAEKJOON/Bronze/P28352번_10/Main.java
@@ -0,0 +1,16 @@
+package P28352번_10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    long N = Long.parseLong(br.readLine());
+    for (int i = 1, R = (int) N; i < R; i++) {
+      N *= i;
+    }
+    System.out.println(N / 604800);
+  }
+}
