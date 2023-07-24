13f219e677ab5139dfa9465132efb7df22ac2385
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-07-24 18:51:34 +0900

    2023-07-24

---
+++ BAEKJOON/Bronze/P26531번_Simple_Sum/Main.java
@@ -0,0 +1,17 @@
+package P26531번_Simple_Sum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String a = br.readLine();
+    if (a.charAt(0) + a.charAt(4) - '0' == a.charAt(8)) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
+}
