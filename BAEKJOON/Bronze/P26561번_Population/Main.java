af3b9b515e3d4a7658581144da9ffc91562d207e
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-07-29 01:59:31 +0900

    2023-07-29

---
+++ BAEKJOON/Bronze/P26561번_Population/Main.java
@@ -0,0 +1,20 @@
+package P26561번_Population;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < n; i++) {
+      String[] input = br.readLine().split(" ");
+      int p = Integer.parseInt(input[0]);
+      int t = Integer.parseInt(input[1]);
+      sb.append(p - (t / 7) + (t / 4)).append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
