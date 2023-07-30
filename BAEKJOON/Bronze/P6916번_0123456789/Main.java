c9b3f624249b94bbda998d0ddb36143a06644f28
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-07-31 01:01:38 +0900

    2023-07-31

---
+++ BAEKJOON/Bronze/P6916번_0123456789/Main.java
@@ -0,0 +1,53 @@
+package P6916번_0123456789;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    if (n == 0 || n == 2 || n == 3 || n == 5 || n == 6 || n == 7 || n == 8 || n == 9) {
+      sb.append(" * * *").append('\n');
+    } else {
+      sb.append('\n');
+    }
+    if (n == 0 || n == 4 || n == 8 || n == 9) {
+      for (int i = 0; i < 3; i++) {
+        sb.append("*     *").append('\n');
+      }
+    } else if (n == 1 || n == 2 || n == 3 || n == 7) {
+      for (int i = 0; i < 3; i++) {
+        sb.append("      *").append('\n');
+      }
+    } else {
+      for (int i = 0; i < 3; i++) {
+        sb.append("*").append('\n');
+      }
+    }
+    if (n == 0 || n == 1 || n == 7) {
+      sb.append('\n');
+    } else {
+      sb.append(" * * *").append('\n');
+    }
+    if (n == 0 || n == 6 || n == 8) {
+      for (int i = 0; i < 3; i++) {
+        sb.append("*     *").append('\n');
+      }
+    } else if (n == 1 || n == 3 || n == 4 || n == 5 || n == 7 || n == 9) {
+      for (int i = 0; i < 3; i++) {
+        sb.append("      *").append('\n');
+      }
+    } else {
+      for (int i = 0; i < 3; i++) {
+        sb.append("*").append('\n');
+      }
+    }
+    if (n == 0 || n == 2 || n == 3 || n == 5 || n == 6 || n == 8 || n == 9) {
+      sb.append(" * * *").append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
