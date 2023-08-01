d4deced44686c0923aab4fe8e4f221ea5fbb94d0
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-08-02 05:07:45 +0900

    2023-08-02

---
+++ BAEKJOON/Bronze/P25858번_Divide_the_Cash/Main.java
@@ -0,0 +1,25 @@
+package P25858번_Divide_the_Cash;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] input = br.readLine().split(" ");
+    int n = Integer.parseInt(input[0]);
+    int d = Integer.parseInt(input[1]);
+    int t = 0;
+    int[] ps = new int[n];
+    for (int i = 0; i < n; i++) {
+      t += ps[i] = Integer.parseInt(br.readLine());
+    }
+    int s = d / t;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      sb.append(s * ps[i]).append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
---
+++ BAEKJOON/Bronze/P28248번_Deliv_e_droid/Main.java
@@ -0,0 +1,15 @@
+package P28248번_Deliv_e_droid;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int P = Integer.parseInt(br.readLine());
+    int C = Integer.parseInt(br.readLine());
+    int F = P > C ? (P * 50) - (C * 10) + 500 : (P * 50) - (C * 10);
+    System.out.println(F);
+  }
+}
