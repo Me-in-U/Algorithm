bedd3e2fa63be8bb8e6e82d774f71a755e96f7bd
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-08-05 05:28:57 +0900

    2023-08-05

---
+++ BAEKJOON/Bronze/P28224번_Final_Price/Main.java
@@ -0,0 +1,17 @@
+package P28224번_Final_Price;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int sum = 0;
+    while (n-- > 0) {
+      sum += Integer.parseInt(br.readLine());
+    }
+    System.out.println(sum);
+  }
+}
---
+++ BAEKJOON/Bronze/P9772번_Quadrants/Main.java
@@ -0,0 +1,30 @@
+package P9772번_Quadrants;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    String[] xy = br.readLine().split(" ");
+    while (!(Double.parseDouble(xy[0]) == 0 && Double.parseDouble(xy[1]) == 0)) {
+      double x = Double.parseDouble(xy[0]);
+      double y = Double.parseDouble(xy[1]);
+      if (x == 0 || y == 0) {
+        sb.append("AXIS").append('\n');
+      } else if (0 < x && 0 < y) {
+        sb.append("Q1").append('\n');
+      } else if (x < 0 && 0 < y) {
+        sb.append("Q2").append('\n');
+      } else if (x < 0 && y < 0) {
+        sb.append("Q3").append('\n');
+      } else {
+        sb.append("Q4").append('\n');
+      }
+      xy = br.readLine().split(" ");
+    }
+    sb.append("AXIS").append('\n');
+    System.out.print(sb.toString());
+  }
+}
