66433c743f33c8c66ee79ae6f4d0bfa92e2ee718
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-09-12 00:11:02 +0900

    2023-09-12

---
+++ BAEKJOON/Bronze/Bronze_V/P29863번_Arrnos_Sleep_Schedule/Main.java
@@ -0,0 +1,20 @@
+package BAEKJOON.Bronze.Bronze_V.P29863번_Arrnos_Sleep_Schedule;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int result = 0;
+    int T = Integer.parseInt(br.readLine());
+    if (20 <= T && T <= 23) {
+      result = 24 - T;
+      result += Integer.parseInt(br.readLine());
+    } else {
+      result = Integer.parseInt(br.readLine()) - T;
+    }
+    System.out.println(result);
+  }
+}
