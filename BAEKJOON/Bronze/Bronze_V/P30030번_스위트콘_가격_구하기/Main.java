64a0b1fbec90cf42af86f758fb72c966cf21b8e8
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-09-19 04:34:39 +0900

    2023-09-19

---
+++ BAEKJOON/Bronze/Bronze_V/P30030번_스위트콘_가격_구하기/Main.java
@@ -0,0 +1,12 @@
+package BAEKJOON.Bronze.Bronze_V.P30030번_스위트콘_가격_구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println((Integer.parseInt(br.readLine()) * 10) / 11);
+  }
+}
--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Main {
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+  public static void main(String[] args) {
+    System.out.println("안녕하세요~");
   }
 }
