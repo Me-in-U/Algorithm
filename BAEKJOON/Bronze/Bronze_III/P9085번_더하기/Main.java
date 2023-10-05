8992ed1fa1a455b23101d162f0f604bb4d1b99dd
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-10-06 05:17:30 +0900

    2023-10-06

---
+++ BAEKJOON/Bronze/Bronze_III/P9085번_더하기/Main.java
@@ -0,0 +1,25 @@
+package BAEKJOON.Bronze.Bronze_III.P9085번_더하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    StringTokenizer st = null;
+    int T = Integer.parseInt(br.readLine());
+    for (int i = 0; i < T; i++) {
+      int N = Integer.parseInt(br.readLine());
+      int sum = 0;
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        sum += Integer.parseInt(st.nextToken());
+      }
+      sb.append(sum).append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
