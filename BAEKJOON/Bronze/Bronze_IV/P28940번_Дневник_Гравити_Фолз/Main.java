cb967f2de19b5c02898f2ef8296351a6c5ae3b9d
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-10-18 04:55:08 +0900

    2023-10-18

---
+++ BAEKJOON/Bronze/Bronze_IV/P28940번_Дневник_Гравити_Фолз/Main.java
@@ -0,0 +1,27 @@
+package BAEKJOON.Bronze.Bronze_IV.P28940번_Дневник_Гравити_Фолз;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = null;
+    st = new StringTokenizer(br.readLine());
+    int w = Integer.parseInt(st.nextToken());
+    int h = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    if (w < a || h < b) {
+      System.out.println("-1");
+      return;
+    }
+    int lettersPerPage = (w / a) * (h / b);
+    int totalPages = (int) Math.ceil((double) n / lettersPerPage);
+    System.out.println(totalPages);
+  }
+}
