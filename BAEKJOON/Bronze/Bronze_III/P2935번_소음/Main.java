0a4594e55805fe1c6a3f29f818e1e9affcf54241
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-10-21 05:42:28 +0900

    2023-10-21

---
+++ BAEKJOON/Bronze/Bronze_III/P2935번_소음/Main.java
@@ -0,0 +1,20 @@
+package BAEKJOON.Bronze.Bronze_III.P2935번_소음;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BigInteger b1 = new BigInteger(br.readLine());
+    char c = br.readLine().charAt(0);
+    BigInteger b2 = new BigInteger(br.readLine());
+    if (c == '+') {
+      System.out.println(b1.add(b2).toString());
+    } else {
+      System.out.println(b1.multiply(b2).toString());
+    }
+  }
+}
