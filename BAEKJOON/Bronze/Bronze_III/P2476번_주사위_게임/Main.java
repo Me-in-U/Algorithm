7d65f646e611986678a5886a3f40f9685591b7a8
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-10-23 04:52:41 +0900

    2023-10-23

---
+++ BAEKJOON/Bronze/Bronze_III/P2476번_주사위_게임/Main.java
@@ -0,0 +1,31 @@
+package BAEKJOON.Bronze.Bronze_III.P2476번_주사위_게임;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int max = 0;
+    for (int i = 0; i < N; i++) {
+      String line = br.readLine();
+      int a = line.charAt(0) - '0';
+      int b = line.charAt(2) - '0';
+      int c = line.charAt(4) - '0';
+      if (a == b && b == c) {
+        max = Math.max(max, 10000 + a * 1000);
+      } else if (a == b) {
+        max = Math.max(max, 1000 + a * 100);
+      } else if (b == c) {
+        max = Math.max(max, 1000 + b * 100);
+      } else if (c == a) {
+        max = Math.max(max, 1000 + c * 100);
+      } else {
+        max = Math.max(max, Math.max(a, Math.max(b, c)) * 100);
+      }
+    }
+    System.out.println(max);
+  }
+}
---
+++ BAEKJOON/Bronze/Bronze_III/P4504번_배수_찾기/Main.java
@@ -0,0 +1,24 @@
+package BAEKJOON.Bronze.Bronze_III.P4504번_배수_찾기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int n = Integer.parseInt(br.readLine());
+    int a = -1;
+    while ((a = Integer.parseInt(br.readLine())) != 0) {
+      sb.append(a);
+      if (a % n == 0) {
+        sb.append(" is a multiple of ");
+      } else {
+        sb.append(" is NOT a multiple of ");
+      }
+      sb.append(n).append(".\n");
+    }
+    System.out.print(sb.toString());
+  }
+}
