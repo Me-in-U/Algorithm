8338ab2013191de31de00cd8c81c9f5e16204c39
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-10-24 01:32:39 +0900

    2023-10-24

---
+++ BAEKJOON/Bronze/Bronze_III/P10102번_개표/Main.java
@@ -0,0 +1,21 @@
+package BAEKJOON.Bronze.Bronze_III.P10102번_개표;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int V = Integer.parseInt(br.readLine());
+    String input = br.readLine();
+    int A = 0;
+    for (int i = 0; i < V; i++)
+      if (input.charAt(i) == 'A')
+        A++;
+    if (A == V - A)
+      System.out.println("Tie");
+    else
+      System.out.println(A > V - A ? "A" : "B");
+  }
+}
