a5061e5ce49f00b7c234ccade20dd0b582dc356d
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-10-26 04:54:55 +0900

    2023-10-26

---
+++ BAEKJOON/Bronze/Bronze_III/P5988번_홀수일까_짝수일까/Main.java
@@ -0,0 +1,18 @@
+package BAEKJOON.Bronze.Bronze_III.P5988번_홀수일까_짝수일까;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    for (int i = 0; i < N; i++) {
+      String num = br.readLine();
+      sb.append((num.charAt(num.length() - 1) - '0') % 2 == 0 ? "even" : "odd").append('\n');
+    }
+    System.out.print(sb.toString());
+  }
+}
