842b41c3ee57f724498fca05d6c8a08e4fceed61
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-11-02 00:42:21 +0900

    2023-10-02

---
+++ BAEKJOON/Bronze/Bronze_III/P10178번_할로윈의_사탕/Main.java
@@ -0,0 +1,24 @@
+package BAEKJOON.Bronze.Bronze_III.P10178번_할로윈의_사탕;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            sb.append("You get ").append(c / v)
+                    .append(" piece(s) and your dad gets ").append(c % v)
+                    .append(" piece(s).").append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
