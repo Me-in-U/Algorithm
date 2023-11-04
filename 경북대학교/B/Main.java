4ef32bb265ce2a109e43cf4be0d2abb492dbf01b
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-11-04 18:27:26 +0900

    2023-11-04

---
+++ 경북대학교/B/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int B_L = B / 10;
+        if (!(B_L == 1 || B_L == 3 || B_L == 7 || B_L == 9)) {
+            System.out.println(-1);
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(A);
+        sb.append(A % 10 == 9 ? 7 : 1);
+        for (int i = 0; i < N - 5; i++)
+            sb.append(1);
+        sb.append(B);
+        System.out.print(sb.toString());
+    }
+}
