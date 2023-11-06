9d67606be15f7cadfd7a567ef537eba494bb8503
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-11-06 21:06:50 +0900

    2023-11-05

---
+++ 건국대/C/Main.java
@@ -0,0 +1,22 @@
+package 건국대.C;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        String result = "";
+        if (N == 0) {
+            result = "1" + "0".repeat(L - 1);
+        } else {
+            result = N + "1".repeat(L - 1);
+        }
+        System.out.println(result);
+    }
+}
