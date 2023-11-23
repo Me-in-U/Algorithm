7fb40273f2b8b70ea05b88691e54e8d9fe2a227e
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-11-24 04:49:26 +0900

    2023-11-24

---
+++ BAEKJOON/Bronze/Bronze_III/P11006번_남욱이의_닭장/Main.java
@@ -0,0 +1,24 @@
+package BAEKJOON.Bronze.Bronze_III.P11006번_남욱이의_닭장;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+        int TCase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < TCase; i++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int U = (M * 2) - N;
+            sb.append(U).append(' ').append(M - U).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
