8f1ad4e56454b287228e7db355a5a90417e97223
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-12-01 18:06:43 +0900

    2023-12-01

--- Test.java
+++ Test.java
@@ -1,20 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = """
-                asdadasdasdasdassadsssssssssssss
-                ASdasda,wsg
-                asdadasdasdasdassadsssssssssssss                asdadasdasdasdassadsssssssssssss
-                asdadasdasdasdassadsssssssssssss
-                asdadasdasdasdassadsssssssssssss
-
-                wsg
-                """;
-        var a = 'd';
-        System.out.print(line);
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++)
+            if (Integer.parseInt(st.nextToken()) != i)
+                count++;
+        System.out.print(count);
     }
 }
