69c19e40650b738f6f9e5ae6daf8af6da528acf0
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-12-21 00:20:10 +0900

    2023-12-21

---
+++ BAEKJOON/Bronze/Bronze_II/P1550번_16진수/Main.java
@@ -0,0 +1,11 @@
+package BAEKJOON.Bronze.Bronze_II.P1550번_16진수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine(), 16));
+    }
+}
