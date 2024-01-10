7738f590c02bc561ef03797210ba31be0c23cc77
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-01-11 03:13:07 +0900

    2024-01-11

---
+++ BAEKJOON/Silver/Silver_V/P15688번_수_정렬하기_5/Main.java
@@ -0,0 +1,21 @@
+package BAEKJOON.Silver.Silver_V.P15688번_수_정렬하기_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++)
+            numbers[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(numbers);
+        for (int i = 0; i < N; i++)
+            sb.append(numbers[i]).append("\n");
+        System.out.println(sb);
+    }
+}
