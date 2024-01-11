1b346b8b625ccf3dc3252a80ca9addcddcf45d98
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-01-12 03:51:21 +0900

    2024-01-12

---
+++ BAEKJOON/Bronze/Bronze_II/P2702번_초6_수학/Main.java
@@ -0,0 +1,27 @@
+package BAEKJOON.Bronze.Bronze_II.P2702번_초6_수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            int gcd = GCD(a, b);
+            sb.append(a * b / gcd).append(' ').append(gcd).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+
+    public static int GCD(int a, int b) {
+        if (a % b == 0)
+            return b;
+        return GCD(b, a % b);
+    }
+}
