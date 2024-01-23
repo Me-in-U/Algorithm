71b7058f764e830621d9c3b6ec2cd1fb045ccff7
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-01-24 05:37:05 +0900

    2024-01-24

---
+++ BAEKJOON/Bronze/Bronze_IV/P31090번_2023은_무억이_특별할까/Main.java
@@ -0,0 +1,18 @@
+package BAEKJOON.Bronze.Bronze_IV.P31090번_2023은_무억이_특별할까;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append((N + 1) % (N % 100) == 0 ? "Good" : "Bye").append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
