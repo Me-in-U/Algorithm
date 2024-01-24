73e9540b66422e4c1157dc00b045d0ef5329f8bb
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-01-25 05:22:30 +0900

    2024-01-25

---
+++ BAEKJOON/Bronze/Bronze_III/P4766번_일반_화학_실험/Main.java
@@ -0,0 +1,19 @@
+package BAEKJOON.Bronze.Bronze_III.P4766번_일반_화학_실험;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        double previous_data = Double.parseDouble(br.readLine());
+        double next_data = 0;
+        while ((next_data = Double.parseDouble(br.readLine())) != 999) {
+            sb.append(String.format("%.2f", next_data - previous_data)).append('\n');
+            previous_data = next_data;
+        }
+        System.out.print(sb.toString());
+    }
+}
