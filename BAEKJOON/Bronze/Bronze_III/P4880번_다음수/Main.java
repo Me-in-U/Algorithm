ddd5f1b0bd7abf94a2234221cfe0ccf09b3e8594
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-01-29 05:40:14 +0900

    2024-01-29

---
+++ BAEKJOON/Bronze/Bronze_III/P4880번_다음수/Main.java
@@ -0,0 +1,30 @@
+package BAEKJOON.Bronze.Bronze_III.P4880번_다음수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String[] input = br.readLine().split(" ");
+            int a1 = Integer.parseInt(input[0]);
+            int a2 = Integer.parseInt(input[1]);
+            int a3 = Integer.parseInt(input[2]);
+            if (a1 == 0 && a2 == 0 && a3 == 0) {
+                break;
+            }
+            int sub1 = a2 - a1;
+            int sub2 = a3 - a2;
+            if (sub1 == sub2) {
+                sb.append("AP ").append(a3 + sub1);
+            } else {
+                sb.append("GP ").append(a3 * (a3 / a2));
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
---
+++ BAEKJOON/Bronze/Bronze_III/P6131번_완전_제곱수/Main.java
@@ -0,0 +1,20 @@
+package BAEKJOON.Bronze.Bronze_III.P6131번_완전_제곱수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        for (int B = 1; B <= 500; B++) {
+            int BBN = (B * B) + N;
+            int A = (int) Math.sqrt(BBN);
+            if (A * A == BBN)
+                count++;
+        }
+        System.out.println(count);
+    }
+}
