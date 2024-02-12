2dfb2bdc1e7d652b61b87c9b9c90977b8b8bed2d
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-02-13 05:26:40 +0900

    2024-02-13

---
+++ BAEKJOON/Bronze/Bronze_IV/P31306번_Is_Y_a_Vowel/Main.java
@@ -0,0 +1,21 @@
+package BAEKJOON.Bronze.Bronze_IV.P31306번_Is_Y_a_Vowel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int v = 0;
+        int y = 0;
+        String w = new BufferedReader(new InputStreamReader(System.in)).readLine();
+        for (int i = 0; i < w.length(); i++) {
+            char c = w.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                v++;
+            else if (c == 'y')
+                y++;
+        }
+        System.out.println(new StringBuilder().append(v).append(' ').append(v + y));
+    }
+}
