19939ccad3338b0f17f8090586fe1b52fcba6b17
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-02-27 05:53:56 +0900

    2024-02-27

---
+++ BAEKJOON/Bronze/Bronze_I/P11284번_초성_중성_종성/Main.java
@@ -0,0 +1,25 @@
+package BAEKJOON.Bronze.Bronze_I.P11284번_초성_중성_종성;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] initialSoundData = { "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ",
+                "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+        String[] middleSoundData = { "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ",
+                "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ" };
+        String[] finalSoundData = { "\n", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ",
+                "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+        char single = br.readLine().charAt(0);
+        int initialSound = ((single - 0xAC00) / 28) / 21;
+        int middleSound = ((single - 0xAC00) / 28) % 21;
+        int finalSound = ((single - 0xAC00)) % 28;
+        StringBuilder sb = new StringBuilder();
+        sb.append(initialSoundData[initialSound]).append('\n').append(middleSoundData[middleSound]).append('\n')
+                .append(finalSoundData[finalSound]);
+        System.out.print(sb.toString());
+    }
+}
---
+++ BAEKJOON/Bronze/Bronze_II/P25501번_재귀의_귀재/Main.java
@@ -0,0 +1,33 @@
+package BAEKJOON.Bronze.Bronze_II.P25501번_재귀의_귀재;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int recursionCount;
+    static String S;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            S = br.readLine();
+            recursionCount = 0;
+            sb.append(isPalindrome(0, S.length() - 1)).append(' ').append(recursionCount).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+
+    public static int isPalindrome(int r, int l) {
+        recursionCount++;
+        if (r >= l) {
+            return 1;
+        } else if (S.charAt(r) != S.charAt(l)) {
+            return 0;
+        } else {
+            return isPalindrome(r + 1, l - 1);
+        }
+    }
+}
