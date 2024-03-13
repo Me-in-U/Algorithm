c93688bdd17967eae8ad233d5ced1f90d66a46ed
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-03-13 10:04:56 +0900

    2024-03-13
    코드 수정

--- BAEKJOON/Silver/Silver_IV/P1940번_주몽/Main.java
+++ BAEKJOON/Silver/Silver_IV/P1940번_주몽/Main.java
@@ -4,13 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        int N = Integer.parseInt(br.readLine());
+        br.readLine();
         int M = Integer.parseInt(br.readLine());
         String data = br.readLine();
         if (M == 1 || M >= 200000) {
