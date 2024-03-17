6f31435d37d75a4e6c7351dfb630b20a22423a61
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-03-18 05:19:22 +0900

    2024-03-18

---
+++ BAEKJOON/Bronze/Bronze_V/P31614번_Minutes/Main.java
@@ -0,0 +1,12 @@
+package BAEKJOON.Bronze.Bronze_V.P31614번_Minutes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(60 * Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
+    }
+}
--- BAEKJOON/Silver/Silver_I/P1389번_케빈_베이컨의_6단계/Main.java
+++ BAEKJOON/Silver/Silver_I/P1389번_케빈_베이컨의_6단계/Main.java
@@ -7,69 +7,46 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int N, M;
-  static boolean[][] connection;
-  static int[] result;
-  static int[][] min_count;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = null;
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    // 입력
-    st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    result = new int[N];
-    min_count = new int[N][N];
-    connection = new boolean[N][N];
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    int[][] arr = new int[N][N];
     for (int i = 0; i < N; i++) {
-      Arrays.fill(min_count[i], 100);
+      Arrays.fill(arr[i], 9999);
+      arr[i][i] = 0;
     }
-    for (int i = 0; i < M; i++) {
+
+    while (M-- > 0) {
       st = new StringTokenizer(br.readLine());
-      int x = Integer.parseInt(st.nextToken());
-      int y = Integer.parseInt(st.nextToken());
-      connection[x - 1][y - 1] = true;
-      connection[y - 1][x - 1] = true;
+      int x = Integer.parseInt(st.nextToken()) - 1;
+      int y = Integer.parseInt(st.nextToken()) - 1;
+      arr[x][y] = arr[y][x] = 1;
     }
 
-    // 계산
+    for (int k = 0; k < N; k++)
+      for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+          if (arr[i][j] > arr[i][k] + arr[k][j])
+            arr[i][j] = arr[i][k] + arr[k][j];
+
+    int minimumDegrees = 9999;
+    int result = -1;
     for (int i = 0; i < N; i++) {
-      min_count[i][i] = 0;
-      dfs(i, i, 1);
-      int sum = 0;
+      int totalDegrees = 0;
       for (int j = 0; j < N; j++) {
-        // System.out.print(min_count[i][j] + " ");
-        sum += min_count[i][j];
+        totalDegrees += arr[i][j];
       }
-      // System.out.println();
-      result[i] = sum;
+      if (minimumDegrees > totalDegrees) {
+        minimumDegrees = totalDegrees;
+        result = i;
+      }
     }
 
-    // 출력
-    int min = 100;
-    int min_person = -1;
-    for (int i = 0; i < N; i++) {
-      if (min > result[i]) {
-        min = result[i];
-        min_person = i + 1;
-      }
-    }
-    System.out.println(min_person);
-  }
-
-  public static void dfs(int now, int start, int depth) {
-    for (int i = 0; i < N; i++) {
-      // System.out.println("not" + "[" + start + "][" + i + "]" + "depth = " +
-      // depth);
-      if (connection[start][i] && min_count[now][i] > depth) {
-        // System.out.println(now + "의 " + "[" + i + "번째]" + "depth = " + depth);
-        min_count[now][i] = depth;
-        // System.out.println();
-        dfs(now, i, depth + 1);
-      }
-    }
-    // System.out.println();
+    System.out.println(result + 1);
   }
 }
