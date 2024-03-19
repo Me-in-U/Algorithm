faa47888cbd24b2fddb448e4ad4322e3635f83aa
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-03-20 04:31:30 +0900

    2024-03-20

---
+++ BAEKJOON/Bronze/Bronze_V/P31611번_Tuesday/Main.java
@@ -0,0 +1,12 @@
+package BAEKJOON.Bronze.Bronze_V.P31611번_Tuesday;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) % 7 == 2 ? "1" : "0");
+    }
+}
