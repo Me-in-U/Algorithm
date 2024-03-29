8b66f4fce0760f8bb87ea17a368f1a35e6d31a91
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-03-29 16:06:32 +0900

    2024-03-29

---
+++ BAEKJOON/Bronze/Bronze_IV/P31612번_Stroke_Count/Main.java
@@ -0,0 +1,23 @@
+package BAEKJOON.Bronze.Bronze_IV.P31612번_Stroke_Count;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            char c = S.charAt(i);
+            if (c == 'j' || c == 'i') {
+                sum += 2;
+            } else if (c == 'o') {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
+}
