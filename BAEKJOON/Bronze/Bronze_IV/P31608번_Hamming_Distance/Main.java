6f75906a3bdf6f0fc003c15e6de455f7d015aefc
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-04-04 04:18:31 +0900

    2024-04-04

---
+++ BAEKJOON/Bronze/Bronze_IV/P31608번_Hamming_Distance/Main.java
@@ -0,0 +1,19 @@
+package BAEKJOON.Bronze.Bronze_IV.P31608번_Hamming_Distance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        int count = 0;
+        for (int i = 0; i < N; i++)
+            if (s1.charAt(i) != s2.charAt(i))
+                count++;
+        System.out.println(count);
+    }
+}
