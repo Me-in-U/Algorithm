513f4d384564ce6f3aad4d7cda2e0131b3255f08
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-04-05 23:11:01 +0900

    2024-04-05

---
+++ BAEKJOON/Bronze/Bronze_IV/P31616번_Matched_Letters/Main.java
@@ -0,0 +1,22 @@
+package BAEKJOON.Bronze.Bronze_IV.P31616번_Matched_Letters;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        char c = S.charAt(0);
+        boolean allSame = true;
+        for (int i = 1; i < N; i++) {
+            if (S.charAt(i) != c) {
+                allSame = false;
+                break;
+            }
+        }
+        System.out.println(allSame ? "Yes" : "No");
+    }
+}
