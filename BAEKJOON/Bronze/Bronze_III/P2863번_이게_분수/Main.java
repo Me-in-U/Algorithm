d298b6a7dc500426869205dc93d4760386c18bf4
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2024-04-16 04:51:21 +0900

    2024-04-16

---
+++ BAEKJOON/Bronze/Bronze_III/P2863번_이게_분수/Main.java
@@ -0,0 +1,33 @@
+package BAEKJOON.Bronze.Bronze_III.P2863번_이게_분수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        double C = Double.parseDouble(st.nextToken());
+        double D = Double.parseDouble(st.nextToken());
+        double[] rotated = {
+                (A / C) + (B / D),
+                (C / D) + (A / B),
+                (D / B) + (C / A),
+                (B / A) + (D / C)
+        };
+        double max = 0;
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            if (max < rotated[i]) {
+                max = rotated[i];
+                count = i;
+            }
+        }
+        System.out.println(count);
+    }
+}
