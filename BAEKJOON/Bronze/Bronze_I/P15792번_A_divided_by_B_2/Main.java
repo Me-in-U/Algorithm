a331b481b360720b9fdca626678919cd9fdabf74
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-04-22 11:56:51 +0900

    2024-04-22

---
+++ BAEKJOON/Bronze/Bronze_I/P15792번_A_divided_by_B_2/Main.java
@@ -0,0 +1,24 @@
+package BAEKJOON.Bronze.Bronze_I.P15792번_A_divided_by_B_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        sb.append(A / B).append('.');
+        A = 10 * (A - B * (A / B));
+        int T = 1000;
+        while (T-- > 0) {
+            sb.append(A / B);
+            A = 10 * (A - B * (A / B));
+        }
+        System.out.println(sb.toString());
+    }
+}
