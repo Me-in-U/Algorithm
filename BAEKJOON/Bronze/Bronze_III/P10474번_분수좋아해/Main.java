cb0a3fb611bc60e3170baf3b134e972fe9c74305
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-05-04 04:14:14 +0900

    2024-05-04

---
+++ BAEKJOON/Bronze/Bronze_III/P10474번_분수좋아해/Main.java
@@ -0,0 +1,24 @@
+package BAEKJOON.Bronze.Bronze_III.P10474번_분수좋아해;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0) {
+                break;
+            }
+            sb.append(a / b).append(' ').append(a % b).append(" / ").append(b).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
