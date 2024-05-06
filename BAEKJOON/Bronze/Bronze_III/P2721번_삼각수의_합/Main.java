f669f2abc37a85f27b83ed357e6eb1af7dad1b6c
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-05-07 04:27:22 +0900

    2024-05-07

---
+++ BAEKJOON/Bronze/Bronze_III/P2721번_삼각수의_합/Main.java
@@ -0,0 +1,21 @@
+package BAEKJOON.Bronze.Bronze_III.P2721번_삼각수의_합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int result = 0;
+            for (int j = 1; j <= n; j++)
+                result += j * (j + 1) * (j + 2) / 2;
+            sb.append(result).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
