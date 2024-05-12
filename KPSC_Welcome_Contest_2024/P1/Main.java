2dd78164d429975ad778b4d8b57240c08e9e56a3
KIM MINGYU <jun3021303@gmail.com>
2024-05-13 00:34:02 +0900

    2024-05-13

--- KPSC_Welcome_Contest_2024/P1/Main.java
+++ KPSC_Welcome_Contest_2024/P1/Main.java
@@ -7,6 +7,17 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        int N = Integer.parseInt(br.readLine());
+        int[] prices = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            prices[i] = Integer.parseInt(br.readLine());
+        }
+        int sum = 0;
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int menu = Integer.parseInt(br.readLine());
+            sum += prices[menu];
+        }
+        System.out.println(sum);
     }
 }
--- KPSC_Welcome_Contest_2024/P2/Main.java
+++ KPSC_Welcome_Contest_2024/P2/Main.java
@@ -7,6 +7,14 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        int count = 0;
+        String reStart = br.readLine().substring(0, 5);
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            if (reStart.equals(br.readLine().substring(0, 5))) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
