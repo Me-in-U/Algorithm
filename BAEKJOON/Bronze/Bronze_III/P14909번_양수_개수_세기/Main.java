f4c3d9e1b210b06675ea4b7c87cb1d08d1e68c61
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-05-16 05:35:36 +0900

    2024-05-16

---
+++ BAEKJOON/Bronze/Bronze_III/P14909번_양수_개수_세기/Main.java
@@ -0,0 +1,17 @@
+package BAEKJOON.Bronze.Bronze_III.P14909번_양수_개수_세기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long count = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .filter(a -> a > 0)
+                .count();
+        System.out.println(count);
+    }
+}
