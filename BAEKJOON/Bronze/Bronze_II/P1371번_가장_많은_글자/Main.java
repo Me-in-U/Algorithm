a7c08f47de87de418927b8e3e2c8f8fde6741a5b
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-05-21 08:18:40 +0900

    2024-05-21

---
+++ BAEKJOON/Bronze/Bronze_II/P1371번_가장_많은_글자/Main.java
@@ -0,0 +1,33 @@
+package BAEKJOON.Bronze.Bronze_II.P1371번_가장_많은_글자;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] count = new int[26];
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            for (char i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+                if ('a' <= c) {
+                    count[c - 'a']++;
+                }
+            }
+        }
+        int maxCount = -1;
+        StringBuilder sb = new StringBuilder();
+        for (char i = 0; i < 26; i++) {
+            if (maxCount < count[i]) {
+                maxCount = count[i];
+                sb.setLength(0);
+                sb.append((char) (i + 'a'));
+            } else if (maxCount == count[i]) {
+                sb.append((char) (i + 'a'));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
--- BAEKJOON/Platinum/Platinum_V/P1328번_고층_빌딩/Main.java
+++ BAEKJOON/Platinum/Platinum_V/P1328번_고층_빌딩/Main.java
@@ -1,4 +1,4 @@
-package Platinum_V.P1328번_고등_빌딩;
+package Platinum_V.P1328번_고층_빌딩;
 
 import java.io.BufferedReader;
 import java.io.IOException;
