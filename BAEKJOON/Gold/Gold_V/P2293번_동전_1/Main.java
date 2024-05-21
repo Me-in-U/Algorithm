71b4881a38966c8ec800b09924e07a61720c1715
KIM MINGYU <jun3021303@gmail.com>
2024-05-21 13:08:26 +0900

    2024-05-21

---
+++ BAEKJOON/Gold/Gold_V/P2293번_동전_1/Main.java
@@ -0,0 +1,24 @@
+package BAEKJOON.Gold.Gold_V.P2293번_동전_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++)
+            coins[i] = Integer.parseInt(br.readLine());
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+        for (int i = 0; i < n; i++)
+            for (int j = 1; j <= k; j++)
+                if (coins[i] <= j)
+                    dp[j] += dp[j - coins[i]];
+        System.out.println(dp[k]);
+    }
+}
---
+++ BAEKJOON/Gold/Gold_V/P2294번_동전_2/Main.java
@@ -0,0 +1,27 @@
+package BAEKJOON.Gold.Gold_V.P2294번_동전_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
+        int max = Integer.MAX_VALUE - 1;
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++)
+            coins[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(coins);
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = coins[i]; j <= k; j++)
+                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+        System.out.println(dp[k] == max ? -1 : dp[k]);
+    }
+}
