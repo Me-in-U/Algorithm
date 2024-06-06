78e64a60318c6b4ebceb7990542e165e2135efc6
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-06-07 05:22:26 +0900

    2024-06-07

---
+++ BAEKJOON/Bronze/Bronze_II/P22113번_창영이와_버스/Main.java
@@ -0,0 +1,30 @@
+package BAEKJOON.Bronze.Bronze_II.P22113번_창영이와_버스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] bus = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[][] fee = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                fee[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int sum = 0;
+        for (int i = 1; i < M; i++)
+            sum += fee[bus[i - 1]][bus[i]];
+        System.out.println(sum);
+    }
+}
