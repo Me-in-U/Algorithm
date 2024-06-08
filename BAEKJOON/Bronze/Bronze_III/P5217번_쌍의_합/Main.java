57a14813c100cb57e01b621841fe0c592677afd2
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-06-08 22:15:21 +0900

    2024-06-08

---
+++ BAEKJOON/Bronze/Bronze_III/P5217번_쌍의_합/Main.java
@@ -0,0 +1,26 @@
+package BAEKJOON.Bronze.Bronze_III.P5217번_쌍의_합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append("Pairs for ").append(n).append(": ");
+            int start = 1;
+            for (int k = 0; k < (n - 1) / 2; k++) {
+                if (k != 0)
+                    sb.append(", ");
+                sb.append(start).append(" ").append(n - start);
+                start++;
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
