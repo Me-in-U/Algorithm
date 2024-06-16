3ae0e067da5145350dd25cb45d619d02afbd8ce8
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-06-17 01:59:41 +0900

    2024-06-17

---
+++ BAEKJOON/Bronze/Bronze_III/P10599번_페르시아의_왕들/Main.java
@@ -0,0 +1,22 @@
+package BAEKJOON.Bronze.Bronze_III.P10599번_페르시아의_왕들;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] input = null;
+        while (true) {
+            input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            if (input[0] == 0 && input[1] == 0 && input[2] == 0 && input[3] == 0) {
+                break;
+            }
+            sb.append(input[2] - input[1]).append(' ').append(input[3] - input[0]).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
---
+++ BAEKJOON/Bronze/Bronze_III/P2712번_미국_스타일/Main.java
@@ -0,0 +1,28 @@
+package BAEKJOON.Bronze.Bronze_III.P2712번_미국_스타일;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String[] data = br.readLine().split(" ");
+            double amount = Double.parseDouble(data[0]);
+            String unit = data[1];
+            if (unit.equals("kg"))
+                sb.append(String.format("%.04f lb", amount * 2.2046));
+            else if (unit.equals("g"))
+                sb.append(String.format("%.04f l", amount * 3.7854));
+            else if (unit.equals("l"))
+                sb.append(String.format("%.04f g", amount * 0.2642));
+            else if (unit.equals("lb"))
+                sb.append(String.format("%.04f kg", amount * 0.4536));
+            sb.append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
