87218b50998e1b44a629d5679412688126912ec3
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-07-08 04:32:42 +0900

    2024-07-08

---
+++ BAEKJOON/Gold/Gold_IV/P1915번_가장_큰_정사각형/Main.java
@@ -0,0 +1,27 @@
+package BAEKJOON.Gold.Gold_IV.P1915번_가장_큰_정사각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+        int[][] dp = new int[N][M];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                if (line.charAt(j) == '1') {
+                    dp[i][j] = (i == 0 || j == 0) ? 1
+                            : Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        System.out.println(max * max);
+    }
+}
---
+++ BAEKJOON/Gold/Gold_IV/P1922번_네트워크_연결/Main.java
@@ -0,0 +1,91 @@
+package BAEKJOON.Gold.Gold_IV.P1922번_네트워크_연결;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.StringTokenizer;
+
+class Edge implements Comparable<Edge> {
+    int start;
+    int end;
+    int weight;
+
+    Edge(int start, int end, int weight) {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return weight - o.weight;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Edge edge = (Edge) obj;
+        return start == edge.start && end == edge.end && weight == edge.weight;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end, weight);
+    }
+}
+
+public class Main {
+    protected static int[] parent;
+    protected static ArrayList<Edge> edges;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        edges = new ArrayList<>();
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+        StringTokenizer st = null;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(a, b, c));
+        }
+        Collections.sort(edges);
+        int result = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            Edge edge = edges.get(i);
+            if (find(edge.start) != find(edge.end)) {
+                result += edge.weight;
+                union(edge.start, edge.end);
+            }
+        }
+        System.out.println(result);
+    }
+
+    public static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+}
