3ced12b3b81905def973411a09ab4d1cbdf60cfd
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-07-11 05:26:05 +0900

    2024-07-11

---
+++ BAEKJOON/Bronze/Bronze_III/P23971번_ZOAC_4/Main.java
@@ -0,0 +1,20 @@
+package BAEKJOON.Bronze.Bronze_III.P23971번_ZOAC_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] HWNM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int H = HWNM[0];
+        int W = HWNM[1];
+        int N = HWNM[2];
+        int M = HWNM[3];
+        int r = (int) Math.ceil((double) H / (N + 1));
+        int c = (int) Math.ceil((double) W / (M + 1));
+        System.out.println(r * c);
+    }
+}
