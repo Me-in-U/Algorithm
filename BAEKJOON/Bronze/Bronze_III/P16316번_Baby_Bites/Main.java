5b913e210b1b8fc18edcba802217b7c99d81f762
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-07-12 20:16:58 +0900

    2024-07-12

---
+++ BAEKJOON/Bronze/Bronze_III/P16316번_Baby_Bites/Main.java
@@ -0,0 +1,23 @@
+package BAEKJOON.Bronze.Bronze_III.P16316번_Baby_Bites;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        boolean countWell = true;
+        for (int i = 0; i < n; i++) {
+            if (!input[i].equals("mumble")) {
+                if (Integer.parseInt(input[i]) != i + 1) {
+                    countWell = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(countWell ? "makes sense" : "something is fishy");
+    }
+}
