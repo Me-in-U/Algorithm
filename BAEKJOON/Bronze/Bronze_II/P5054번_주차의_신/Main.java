db9463e7deb102d5673a675fd0c2c94cbdda1fee
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-07-15 13:21:39 +0900

    2024-07-15

---
+++ BAEKJOON/Bronze/Bronze_II/P5054번_주차의_신/Main.java
@@ -0,0 +1,21 @@
+package BAEKJOON.Bronze.Bronze_II.P5054번_주차의_신;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            br.readLine();
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+            sb.append((arr[arr.length - 1] - arr[0]) * 2).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
