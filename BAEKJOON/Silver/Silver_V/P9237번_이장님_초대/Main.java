a1686cb802fdb3f495a46ff81a7b64995cc0d8c6
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-07-17 05:26:11 +0900

    2024-07-17

---
+++ BAEKJOON/Silver/Silver_V/P9237번_이장님_초대/Main.java
@@ -0,0 +1,24 @@
+package BAEKJOON.Silver.Silver_V.P9237번_이장님_초대;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] ti = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++)
+            max = Math.max(max, ti[i] + i + 1);
+        System.out.println(max + 1);
+    }
+}
