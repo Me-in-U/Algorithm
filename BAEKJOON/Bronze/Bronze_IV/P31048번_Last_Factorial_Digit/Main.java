e3a026bf20a1ba4ebc4a0ed95a73507af23d6162
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-07-26 05:11:00 +0900

    2024-07-26

---
+++ BAEKJOON/Bronze/Bronze_IV/P31048번_Last_Factorial_Digit/Main.java
@@ -0,0 +1,19 @@
+package BAEKJOON.Bronze.Bronze_IV.P31048번_Last_Factorial_Digit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] numbers = { 0, 1, 2, 6, 4 };
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(numbers[N < 5 ? N : 0]).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
