70b5a8d326aaf89c38391077b823a3a186f7363b
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-07-27 04:13:27 +0900

    2024-07-27

---
+++ BAEKJOON/Bronze/Bronze_III/P11295번_Exercising/Main.java
@@ -0,0 +1,25 @@
+package BAEKJOON.Bronze.Bronze_III.P11295번_Exercising;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1;; i++) {
+            int stride = Integer.parseInt(br.readLine().trim());
+            if (stride == 0) {
+                break;
+            }
+            sb.append("User ").append(i).append('\n');
+            int N = Integer.parseInt(br.readLine().trim());
+            while (N-- > 0) {
+                double distance = (stride * Integer.parseInt(br.readLine().trim())) / 100000.0;
+                sb.append(String.format("%.5f", distance)).append('\n');
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
