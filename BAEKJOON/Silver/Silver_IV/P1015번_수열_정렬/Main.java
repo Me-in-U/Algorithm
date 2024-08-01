427906fcf1d6b88d82745b9bcff87ad4b9da0f99
KIM MINGYU <jun3021303@gmail.com>
2024-08-02 05:52:39 +0900

    Update Main.java

--- BAEKJOON/Silver/Silver_IV/P1015번_수열_정렬/Main.java
+++ BAEKJOON/Silver/Silver_IV/P1015번_수열_정렬/Main.java
@@ -1,2 +1,27 @@
-package P1015번_수열_정렬;
-import java.io.BufferedReader;import java.io.IOException;import java.io.InputStreamReader;public class Main {public static void main(String[] args) throws IOException {BufferedReader br = new BufferedReader(new InputStreamReader(System.in));}} 
+package BAEKJOON.Silver.Silver_IV.P1015번_수열_정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] B = Arrays.copyOf(A, A.length);
+        Arrays.sort(B);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (A[i] == B[j]) {
+                    sb.append(j + " ");
+                    B[j] = -1;
+                    break;
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
