267e6d74d638d8301d7d16d81829f9280f4baf22
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-08-02 05:44:09 +0900

    2024-08-02

---
+++ BAEKJOON/Bronze/Bronze_III/P31833번_온데간데없을뿐더러/Main.java
@@ -0,0 +1,17 @@
+package BAEKJOON.Bronze.Bronze_III.P31833번_온데간데없을뿐더러;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String A = br.readLine().replace(" ", "");
+        String B = br.readLine().replace(" ", "");
+        Long X = Long.parseLong(A);
+        Long Y = Long.parseLong(B);
+        System.out.println(X > Y ? Y : X);
+    }
+}
---
+++ BAEKJOON/Silver/Silver_IV/P1015번_수열_정렬/Main.java
@@ -0,0 +1,2 @@
+package P1015번_수열_정렬;
+import java.io.BufferedReader;import java.io.IOException;import java.io.InputStreamReader;public class Main {public static void main(String[] args) throws IOException {BufferedReader br = new BufferedReader(new InputStreamReader(System.in));}} 
