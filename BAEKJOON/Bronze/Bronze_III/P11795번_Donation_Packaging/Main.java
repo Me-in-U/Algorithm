71e57192f13ca6bced5f1c3afbf73db9b07a4f8d
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-08-04 04:56:34 +0900

    2024-08-04

---
+++ BAEKJOON/Bronze/Bronze_III/P11795번_Donation_Packaging/Main.java
@@ -0,0 +1,34 @@
+package BAEKJOON.Bronze.Bronze_III.P11795번_Donation_Packaging;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int setA = 0;
+        int setB = 0;
+        int setC = 0;
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            setA += input[0];
+            setB += input[1];
+            setC += input[2];
+            if (setA >= 30 && setB >= 30 && setC >= 30) {
+                int minimum = Math.min(setA, Math.min(setB, setC));
+                setA -= minimum;
+                setB -= minimum;
+                setC -= minimum;
+                sb.append(minimum);
+            } else {
+                sb.append("NO");
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
