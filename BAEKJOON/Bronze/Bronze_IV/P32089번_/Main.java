55709484a4baf6bef889c2f3c2b888f2ee870ddd
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-08-10 21:51:39 +0900

    2024-08-10

---
+++ BAEKJOON/Bronze/Bronze_IV/P32089번_/Main.java
@@ -0,0 +1,25 @@
+package BAEKJOON.Bronze.Bronze_IV.P32089번_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n;
+        while ((n = Integer.parseInt(br.readLine())) != 0) {
+            int[] a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int max = 0;
+            int current = 0;
+            for (int i = 0; i < n; i++) {
+                current += (a[i] - ((3 <= i) ? a[i - 3] : 0));
+                max = Math.max(max, current);
+            }
+            sb.append(max).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
