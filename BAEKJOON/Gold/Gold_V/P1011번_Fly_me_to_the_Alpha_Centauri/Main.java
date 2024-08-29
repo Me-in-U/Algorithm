1eb9451041527c5bc6a682036089a0d879bfab44
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-08-29 20:44:24 +0900

    Update Main.java

--- BAEKJOON/Gold/Gold_V/P1011번_Fly_me_to_the_Alpha_Centauri/Main.java
+++ BAEKJOON/Gold/Gold_V/P1011번_Fly_me_to_the_Alpha_Centauri/Main.java
@@ -3,19 +3,26 @@ package Gold_V.P1011번_Fly_me_to_the_Alpha_Centauri;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-  private static int start, to, dist;
-
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = null;
-
-    int N = Integer.parseInt(br.readLine());
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      start = Integer.parseInt(st.nextToken());
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
+    while (T-- > 0) {
+      int[] XY = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+      int distance = XY[1] - XY[0];
+      int max = (int) Math.sqrt(distance);
+      if ((int) Math.sqrt(distance) == Math.sqrt(distance)) {
+        sb.append(max * 2 - 1);
+      } else if (distance <= max * max + max) {
+        sb.append(max * 2);
+      } else {
+        sb.append(max * 2 + 1);
+      }
+      sb.append('\n');
     }
+    System.out.println(sb.toString().trim());
   }
 }
