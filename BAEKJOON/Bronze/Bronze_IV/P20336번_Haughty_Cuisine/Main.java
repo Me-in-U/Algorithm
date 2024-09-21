25e4ec669903b2a6fef0007664e7cd463c9c085e
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-09-21 10:40:27 +0900

    2024-09-21

--- BAEKJOON/Bronze/Bronze_IV/P20336번_Haughty_Cuisine/Main.java
+++ BAEKJOON/Bronze/Bronze_IV/P20336번_Haughty_Cuisine/Main.java
@@ -3,42 +3,17 @@ package BAEKJOON.Bronze.Bronze_IV.P20336번_Haughty_Cuisine;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int[] graph;
-    static boolean[] visited;
-    static int cycleCount;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        while (T-- > 0) {
-            int N = Integer.parseInt(br.readLine());
-            graph = new int[N + 1];
-            visited = new boolean[N + 1];
-            cycleCount = 0;
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= N; i++) {
-                graph[i] = Integer.parseInt(st.nextToken());
-            }
-            for (int i = 1; i <= N; i++) {
-                if (!visited[i]) {
-                    dfs(i);
-                    cycleCount++;
-                }
-            }
-            sb.append(cycleCount).append("\n");
+        int N = Integer.parseInt(br.readLine());
+        String[][] menuList = new String[N][];
+        for (int i = 0; i < N; i++) {
+            menuList[i] = br.readLine().split(" ");
         }
-        System.out.print(sb.toString());
-    }
-
-    public static void dfs(int start) {
-        visited[start] = true;
-        int next = graph[start];
-        if (!visited[next]) {
-            dfs(next);
+        for (String menu : menuList[0]) {
+            System.out.println(menu);
         }
     }
 }
