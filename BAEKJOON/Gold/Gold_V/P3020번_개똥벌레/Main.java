4018d7c05dc9e57760cac2a7e2f58469cb7d1f0f
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-09-25 15:36:44 +0900

    2024-09-25

--- BAEKJOON/Gold/Gold_V/P3020번_개똥벌레/Main.java
+++ BAEKJOON/Gold/Gold_V/P3020번_개똥벌레/Main.java
@@ -7,7 +7,30 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int H = Integer.parseInt(input[1]);
+        int[] stalactite = new int[H + 1];
+        int[] stalagmite = new int[H + 1];
+        for (int i = 0; i < N; i += 2) {
+            stalactite[Integer.parseInt(br.readLine())]++;
+            stalagmite[Integer.parseInt(br.readLine())]++;
+        }
+        for (int i = H - 1; 0 < i; i--) {
+            stalactite[i] += stalactite[i + 1];
+            stalagmite[i] += stalagmite[i + 1];
+        }
+        int count = 0;
+        int min = N;
+        for (int i = H; 0 < i; i--) {
+            int temp = stalactite[i] + stalagmite[H - i + 1];
+            if (temp < min) {
+                count = 1;
+                min = temp;
+            } else if (temp == min) {
+                count++;
+            }
+        }
+        System.out.println(new StringBuilder().append(min).append(' ').append(count));
     }
 }
---
+++ BAEKJOON/Silver/Silver_III/P3568번_iSharp/Main.java
@@ -0,0 +1,36 @@
+package BAEKJOON.Silver.Silver_III.P3568번_iSharp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+        line = line.substring(0, line.length() - 1).replace(",", "");
+        String[] subs = line.split(" ");
+        for (int i = 1; i < subs.length; i++) {
+            sb.append(subs[0]);
+            StringBuilder name = new StringBuilder();
+            StringBuilder arp = new StringBuilder();
+            for (int j = 0; j < subs[i].length(); j++) {
+                char c = subs[i].charAt(j);
+                if ('a' <= c && c <= 'z' ||
+                        'A' <= c && c <= 'Z') {
+                    name.append(c);
+                } else {
+                    if (c == '[') {
+                        c = ']';
+                    } else if (c == ']') {
+                        c = '[';
+                    }
+                    arp.append(c);
+                }
+            }
+            sb.append(arp.reverse()).append(' ').append(name).append(';').append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
