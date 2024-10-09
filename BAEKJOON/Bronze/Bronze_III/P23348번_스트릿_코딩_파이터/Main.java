070d0619e0fe621d0f694194da17c2b5372b5afa
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-10-10 05:09:36 +0900

    2024-10-10

---
+++ BAEKJOON/Bronze/Bronze_III/P23348번_스트릿_코딩_파이터/Main.java
@@ -0,0 +1,25 @@
+package Bronze_III.P23348번_스트릿_코딩_파이터;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] techScore = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int max = 0;
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            for (int j = 0; j < 3; j++) {
+                String[] input = br.readLine().split(" ");
+                sum += (techScore[0] * Integer.parseInt(input[0])) + (techScore[1] * Integer.parseInt(input[1]))
+                        + (techScore[2] * Integer.parseInt(input[2]));
+            }
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+    }
+}
---
+++ BAEKJOON/Bronze/Bronze_V/P32314번_Chrismas_Tree_Adapter/Main.java
@@ -0,0 +1,16 @@
+package Bronze_V.P32314번_Chrismas_Tree_Adapter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ampere = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int watt = Integer.parseInt(input[0]);
+        int volt = Integer.parseInt(input[1]);
+        System.out.println(ampere * volt <= watt ? "1" : "0");
+    }
+}
