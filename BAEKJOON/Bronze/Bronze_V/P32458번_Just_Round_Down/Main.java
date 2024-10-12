7019bc7e739b451f51d685db61a6448fc40204a6
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-10-13 05:02:53 +0900

    2024-10-13

---
+++ BAEKJOON/Bronze/Bronze_V/P32458번_Just_Round_Down/Main.java
@@ -0,0 +1,13 @@
+package BAEKJOON.Bronze.Bronze_V.P32458번_Just_Round_Down;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int rd = (int) Math.floor(Double.parseDouble(br.readLine()));
+        System.out.println(rd);
+    }
+}
