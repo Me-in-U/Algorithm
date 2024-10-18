92bca1c408b20053ca528f13af14ea74ae03af88
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-10-18 15:54:06 +0900

    2024-10-18

---
+++ BAEKJOON/Silver/Silver_IV/P11508번_2Plus1_세일/Main.java
@@ -0,0 +1,26 @@
+package P11508번_2Plus1_세일;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Integer[] c = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            c[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(c, Collections.reverseOrder());
+        int minCost = 0;
+        for (int i = 0; i < N; i++) {
+            if (i % 3 != 2) {
+                minCost += c[i];
+            }
+        }
+        System.out.println(minCost);
+    }
+}
