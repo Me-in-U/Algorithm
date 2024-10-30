0dc23d06917b88be84eff952c3225a42bae2bf1e
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-10-31 00:01:47 +0900

    2024-10-31

---
+++ BAEKJOON/Bronze/Bronze_III/P26535번_Chicken_Pen/Main.java
@@ -0,0 +1,34 @@
+package P26535번_Chicken_Pen;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int chickens = Integer.parseInt(br.readLine());
+        int sideLength = 1;
+        while (chickens > sideLength * sideLength) {
+            sideLength++;
+        }
+        StringBuilder output = new StringBuilder();
+        StringBuilder mid = new StringBuilder();
+        for (int i = 0; i < sideLength + 2; i++) {
+            output.append('x');
+        }
+        mid.append('x');
+        for (int i = 0; i < sideLength; i++) {
+            mid.append('.');
+        }
+        mid.append('x');
+        output.append('\n');
+        for (int i = 0; i < sideLength; i++) {
+            output.append(mid).append('\n');
+        }
+        for (int i = 0; i < sideLength + 2; i++) {
+            output.append('x');
+        }
+        System.out.print(output.toString());
+    }
+}
