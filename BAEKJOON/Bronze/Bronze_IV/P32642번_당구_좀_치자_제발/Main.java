792471e83078bed4971a218e357655ca4729bbf6
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-11-12 11:14:10 +0900

    2024-11-12

---
+++ BAEKJOON/Bronze/Bronze_IV/P32642번_당구_좀_치자_제발/Main.java
@@ -0,0 +1,25 @@
+package P32642번_당구_좀_치자_제발;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] rain = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        long sum = 0;
+        int anger = 0;
+        for (int i = 0; i < N; i++) {
+            if (rain[i] == 0) {
+                anger--;
+            } else {
+                anger++;
+            }
+            sum += anger;
+        }
+        System.out.println(sum);
+    }
+}
