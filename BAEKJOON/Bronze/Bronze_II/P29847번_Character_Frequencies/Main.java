829232bc54c0605a1cb9ee9df30797414aca3d8b
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-11-16 05:51:42 +0900

    2024-11-16

---
+++ BAEKJOON/Bronze/Bronze_II/P29847번_Character_Frequencies/Main.java
@@ -0,0 +1,29 @@
+package P29847번_Character_Frequencies;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] freq = new int[256];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+                if (c != ' ') {
+                    freq[c]++;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > 0) {
+                sb.append((char) i).append(" ").append(freq[i]).append('\n');
+            }
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
