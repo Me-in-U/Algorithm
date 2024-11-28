73130b11bc82e4078c92e7f50ad2f9e501d9b07e
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-11-29 05:33:19 +0900

    2024-11-29

---
+++ BAEKJOON/Bronze/Bronze_III/P17174번_전체_계산_횟수/Main.java
@@ -0,0 +1,19 @@
+package P17174번_전체_계산_횟수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        int count = N;
+        while (N >= M) {
+            count += (N = (N / M));
+        }
+        System.out.println(count);
+    }
+}
---
+++ BAEKJOON/Bronze/Bronze_IV/P32604번_Junbled_Scoreboards/Main.java
@@ -0,0 +1,29 @@
+package P32604번_Junbled_Scoreboards;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int a = 0;
+        int b = 0;
+        String[] scores = null;
+        boolean chronological = true;
+        while (N-- > 0) {
+            scores = br.readLine().split(" ");
+            if (chronological) {
+                int new_a = Integer.parseInt(scores[0]);
+                int new_b = Integer.parseInt(scores[1]);
+                if (new_a < a || new_b < b) {
+                    chronological = false;
+                }
+                a = new_a;
+                b = new_b;
+            }
+        }
+        System.out.println(chronological ? "yes" : "no");
+    }
+}
