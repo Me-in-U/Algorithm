e1f3296370f5a12a4835b34e6609aa0864f9cb04
KIM MINGYU <44316764+Me-in-U@users.noreply.github.com>
2024-12-02 06:14:17 +0900

    Update Main.java

--- BAEKJOON/Bronze/Bronze_III/P20361번_일우는_야바위꾼/Main.java
+++ BAEKJOON/Bronze/Bronze_III/P20361번_일우는_야바위꾼/Main.java
@@ -1,33 +1,52 @@
 package P20361번_일우는_야바위꾼;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int X = Integer.parseInt(input[1]);
-        int K = Integer.parseInt(input[2]);
-        int[] cups = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            cups[i] = i;
+        BufferedInputStream bis = new BufferedInputStream(System.in);
+        PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+
+        byte[] buffer = new byte[1400021];
+        int len = bis.read(buffer);
+        int pos = 0;
+
+        int X = 0;
+        int K = 0;
+        while (buffer[pos] != ' ') {
+            pos++;
         }
-        for (int i = 0; i < K; i++) {
-            input = br.readLine().split(" ");
-            int A = Integer.parseInt(input[0]);
-            int B = Integer.parseInt(input[1]);
-            if (A == X) {
+        pos++;
+        while (buffer[pos] != ' ') {
+            X = X * 10 + (buffer[pos++] - '0');
+        }
+        pos++;
+        while (buffer[pos] != '\n' && buffer[pos] != '\r') {
+            K = K * 10 + (buffer[pos++] - '0');
+        }
+        pos++;
+        while (K-- > 0) {
+            int A = 0;
+            int B = 0;
+            while (pos < len && buffer[pos] != ' ') {
+                A = A * 10 + (buffer[pos++] - '0');
+            }
+            pos++;
+            while (pos < len && buffer[pos] != '\n' && buffer[pos] != '\r') {
+                B = B * 10 + (buffer[pos++] - '0');
+            }
+            pos++;
+
+            if (X == A) {
                 X = B;
-            } else if (B == X) {
+            } else if (X == B) {
                 X = A;
             }
-            int temp = A;
-            cups[B] = A;
-            cups[A] = temp;
         }
-        System.out.println(X);
+        pw.println(X);
+        pw.flush();
     }
 }
