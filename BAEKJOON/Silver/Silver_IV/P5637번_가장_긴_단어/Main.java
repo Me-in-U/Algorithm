47684df5d3388dc96218ec168666a53bb5e1e46f
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-12-11 03:11:38 +0900

    2024-12-11

---
+++ BAEKJOON/Silver/Silver_IV/P5637번_가장_긴_단어/Main.java
@@ -0,0 +1,15 @@
+package P5637번_가장_긴_단어;
+
+import java.util.*;
+
+interface Main {
+    static void main(String[] g) {
+        Scanner s = new Scanner(System.in);
+        String m = "", l;
+        while (!(l = s.nextLine()).contains("E-N-D"))
+            for (String w : l.replaceAll("[^a-zA-Z-]", " ").split("\\s+"))
+                if (w.length() > m.length())
+                    m = w;
+        System.out.print(m.toLowerCase());
+    }
+}
