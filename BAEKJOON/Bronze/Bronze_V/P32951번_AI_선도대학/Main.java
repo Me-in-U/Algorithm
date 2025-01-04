352d609d833d5372d579cfbb351b6f9945dd19a2
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2025-01-05 05:16:47 +0900

    2024-01-05

---
+++ BAEKJOON/Bronze/Bronze_V/P32951번_AI_선도대학/Main.java
@@ -0,0 +1,24 @@
+package P32951번_AI_선도대학;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = readInt();
+        bw.write(N >= 2024 ? String.valueOf(N - 2024) : String.valueOf(2024 - N));
+        bw.flush();
+    }
+
+    public static int readInt() throws IOException {
+        int c;
+        int n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
