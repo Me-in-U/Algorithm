ff3e4e6ce8c5b8999e8181ff960eebe97e0b4a60
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2025-01-07 05:03:33 +0900

    2025-01-07

---
+++ BAEKJOON/Bronze/Bronze_IV/P32902번_Chips/Main.java
@@ -0,0 +1,15 @@
+package P32902번_Chips;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int k = Integer.parseInt(input[0]);
+        int n = Integer.parseInt(input[1]);
+        System.out.println(new StringBuilder().append(n + 1).append(' ').append(k * n + 1));
+    }
+}
