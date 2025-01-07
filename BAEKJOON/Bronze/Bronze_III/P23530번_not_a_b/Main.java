ed6e26880c631ec5e82ac69912940db031a1df80
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2025-01-08 03:01:12 +0900

    2024-01-08

---
+++ BAEKJOON/Bronze/Bronze_III/P23530번_not_a_b/Main.java
@@ -0,0 +1,26 @@
+package P23530번_not_a_b;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int t = readInt();
+        while (t-- > 0) {
+            sb.append(1).append('\n');
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    public static int readInt() throws IOException {
+        int c;
+        int n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n * 10) + (c & 15);
+        return n;
+    }
+}
