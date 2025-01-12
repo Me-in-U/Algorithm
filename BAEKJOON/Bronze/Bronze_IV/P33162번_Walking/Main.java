75b2141879c2ff50077c3733c8ad081886dbc20c
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2025-01-13 04:43:46 +0900

    2025-01-13

---
+++ BAEKJOON/Bronze/Bronze_IV/P33162번_Walking/Main.java
@@ -0,0 +1,21 @@
+package P33162번_Walking;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int x = readInt();
+        int meter = x / 2 + (x % 2 == 1 ? 3 : 0);
+        System.out.println(meter);
+    }
+
+    public static int readInt() throws IOException {
+        int c;
+        int n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n * 10) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
