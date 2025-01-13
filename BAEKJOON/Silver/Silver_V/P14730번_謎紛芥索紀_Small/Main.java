f0f983696e091ef5c9250dc1ce3ff059df81fe40
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2025-01-14 05:08:58 +0900

    2025-01-14

---
+++ BAEKJOON/Silver/Silver_V/P14730번_謎紛芥索紀_Small/Main.java
@@ -0,0 +1,38 @@
+package P14730번_謎紛芥索紀_Small;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = readInt();
+        int result = 0;
+        while (N-- > 0) {
+            int c = readInt();
+            int k = readInt();
+            result += c * k;
+        }
+        bw.write(String.valueOf(result));
+        bw.flush();
+    }
+
+    public static int readInt() throws IOException {
+        int n = 0;
+        int c = System.in.read();
+        boolean isNegative = false;
+        while (c <= 32) {
+            c = System.in.read();
+        }
+        if (c == '-') {
+            isNegative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            n = (n * 10) + (c - '0');
+            c = System.in.read();
+        }
+        return isNegative ? -n : n;
+    }
+}
