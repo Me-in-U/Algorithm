0e016bbad2d183e5c0e46b4accba346b92b5fd6f
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2025-01-22 04:58:26 +0900

    2025-01-22

---
+++ BAEKJOON/Silver/Silver_V/P3155번_라디오/Main.java
@@ -0,0 +1,21 @@
+package P3155번_라디오;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int N = Integer.parseInt(br.readLine());
+        int min = Math.abs(A - B);
+        for (int i = 0; i < N; i++) {
+            int next = Integer.parseInt(br.readLine());
+            min = Math.min(Math.abs(B - next) + 1, min);
+        }
+        System.out.println(min);
+    }
+}
