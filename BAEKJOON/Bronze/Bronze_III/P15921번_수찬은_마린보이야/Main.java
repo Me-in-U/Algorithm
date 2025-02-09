551a0937133fd5d423cacd01d91497f383d080b7
김민규 <jun3021303@naver.com>
2025-02-10 03:22:26 +0900

    2025-02-10

---
+++ BAEKJOON/Bronze/Bronze_III/P15921번_수찬은_마린보이야/Main.java
@@ -0,0 +1,7 @@
+package P15921번_수찬은_마린보이야;
+
+interface Main {
+    static void main(String[] a) throws Exception {
+        System.out.print(System.in.read() == 48 ? "divide by zero" : "1.00");
+    }
+}
