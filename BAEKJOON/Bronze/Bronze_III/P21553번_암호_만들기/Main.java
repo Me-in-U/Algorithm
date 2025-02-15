9fc4343011748f6aec04a710a7b621bb162be683
김민규 <jun3021303@naver.com>
2025-02-16 05:58:35 +0900

    2025-02-16

---
+++ BAEKJOON/Bronze/Bronze_III/P21553번_암호_만들기/Main.java
@@ -0,0 +1,11 @@
+package P21553번_암호_만들기;
+
+interface Main {
+    static void main(String[] a) throws Exception {
+        int c;
+        while (System.in.read() != 10)
+            ;
+        while ((c = System.in.read()) > 47)
+            System.out.print((char) c);
+    }
+}
