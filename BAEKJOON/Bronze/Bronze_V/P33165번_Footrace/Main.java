7a609c0789c91539864959620146da77715fb5c6
김민규 <jun3021303@naver.com>
2025-02-17 05:21:25 +0900

    2024-02-17

---
+++ BAEKJOON/Bronze/Bronze_V/P33165번_Footrace/Main.java
@@ -0,0 +1,12 @@
+package P33165번_Footrace;
+
+interface Main {
+    static void main(String[] a) throws Exception {
+        int c, T = 0, V = 0;
+        while ((c = System.in.read()) > 47)
+            T = T * 10 + (c & 15);
+        while ((c = System.in.read()) > 47)
+            V = V * 10 + (c & 15);
+        System.out.print(T * V);
+    }
+}
