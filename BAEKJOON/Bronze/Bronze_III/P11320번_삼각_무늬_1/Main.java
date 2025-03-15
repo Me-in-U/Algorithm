698f9f6cb6fa21b33c7f0d80a0b3d8de0b75d8e9
김민규(KIM MINGYU) <jun3021303@naver.com>
2025-03-16 03:44:48 +0900

    Create Main.java

---
+++ BAEKJOON/Bronze/Bronze_III/P11320번_삼각_무늬_1/Main.java
@@ -0,0 +1,27 @@
+package P11320번_삼각_무늬_1;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int T = readInt();
+        while (T-- > 0) {
+            int a = readInt();
+            int b = readInt();
+            int result = (a / b) * (a / b);
+            sb.append(result).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+
+    private static int readInt() throws IOException {
+        int c;
+        int n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n * 10) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
