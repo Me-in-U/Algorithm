8a0f21187e815c1c60dfc97cdc3fb3fb14715868
김민규 <jun3021303@naver.com>
2025-03-24 18:00:49 +0900

    2025-03-24

--- BAEKJOON/Silver/Silver_II/P6603번_로또/Main.java
+++ BAEKJOON/Silver/Silver_II/P6603번_로또/Main.java
@@ -1,50 +1,47 @@
 package P6603번_로또;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-  private static int K;
-  private static int[] arr;
-  private static boolean[] visited;
-  private static StringBuilder sb = new StringBuilder();
+    private static int K;
+    private static int[] arr;
+    private static boolean[] visited;
+    private static StringBuilder sb = new StringBuilder();
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = null;
-    while (true) {
-      st = new StringTokenizer(br.readLine());
-      K = Integer.parseInt(st.nextToken());
-      if (K == 0) {
-        break;
-      }
-      arr = new int[K];
-      visited = new boolean[K];
-      for (int i = 0; i < K; i++) {
-        arr[i] = Integer.parseInt(st.nextToken());
-      }
-
-      DFS(0, 0);
-      sb.append('\n');
-    }
-    System.out.print(sb.toString());
-  }
-
-  private static void DFS(int start, int depth) {
-    if (depth == 6) {
-      for (int i = 0; i < K; i++) {
-        if (visited[i]) {
-          sb.append(arr[i] + " ");
+    public static void main(String[] args) throws IOException {
+        while ((K = readInt()) != 0) {
+            arr = new int[K];
+            Arrays.fill(visited, false);
+            for (int i = 0; i < K; i++)
+                arr[i] = readInt();
+            DFS(0, 0);
+            sb.append("\n");
         }
-      }
-      sb.append('\n');
+        System.out.print(sb.toString().trim());
     }
-    for (int i = start; i < K; i++) {
-      visited[i] = true;
-      DFS(i + 1, depth + 1);
-      visited[i] = false;
+
+    private static void DFS(int start, int depth) {
+        if (depth == 6) {
+            for (int i = 0; i < K; i++)
+                if (visited[i])
+                    sb.append(arr[i]).append(" ");
+            sb.append("\n");
+        }
+        for (int i = start; i < K; i++) {
+            visited[i] = true;
+            DFS(i + 1, depth + 1);
+            visited[i] = false;
+        }
     }
-  }
+
+    private static int readInt() throws IOException {
+        int c;
+        int n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n * 10) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
 }
