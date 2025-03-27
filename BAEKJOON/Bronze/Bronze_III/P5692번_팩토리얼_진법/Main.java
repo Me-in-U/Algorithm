6d4287803c65b63757c35783964d1addb2a34848
김민규(KIM MINGYU) <jun3021303@naver.com>
2025-03-27 21:03:02 +0900

    Create Main.java

---
+++ BAEKJOON/Bronze/Bronze_III/P5692번_팩토리얼_진법/Main.java
@@ -0,0 +1,35 @@
+package P5692번_팩토리얼_진법;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int a = readInt();
+            if (a == 0)
+                break;
+            int count = 1;
+            int num = 1;
+            int sum = 0;
+            while (a > 0) {
+                int n = a % 10;
+                sum += n * num;
+                a /= 10;
+                num *= ++count;
+            }
+            sb.append(sum).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+
+    private static int readInt() throws IOException {
+        int c;
+        int n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n * 10) + (c & 15);
+        if (c == 13)
+            System.in.read();
+        return n;
+    }
+}
